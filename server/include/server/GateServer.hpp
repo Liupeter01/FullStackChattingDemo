14830d09ff70ef1fd9891859f7e055a6a182264f
PEIHENG(Jonathan) LIU <Liupeter012001@outlook.com>
2024-07-30 11:35:32 +1000

    1.[ADD]:add gateserver to handle connection request from client 2.[ADD]:using map DS to record unique uuid for each connection with Session unique_ptr 3.[FUNCTION]:estalich http service and handle socket request

---
+++ server/include/server/GateServer.hpp
@@ -0,0 +1,30 @@
+#pragma once
+#ifndef _GATESERVER_HPP_
+#define _GATESERVER_HPP_
+#include<map>
+#include<server/session.hpp>
+
+class GateServer
+          :public std::enable_shared_from_this<GateServer> 
+{
+public:
+          GateServer(boost::asio::io_context& _ioc, unsigned short port);
+          ~GateServer() = default;
+
+public:
+          void serverStart();
+
+private:
+          void handleAccept(std::shared_ptr<Session> session, boost::system::error_code ec);
+          void terminateSession(const std::string& uuid);
+
+private:
+          boost::asio::io_context& m_ioc;
+          boost::asio::ip::tcp::acceptor m_acceptor;
+          std::map<
+                    /*uuid            */std::string, 
+                    /*connections*/std::shared_ptr<Session>
+          > m_sessions;
+};
+
+#endif // !_GATESERVER_HPP_
---
+++ server/src/GateServer.cpp
@@ -0,0 +1,48 @@
+#include<server/GateServer.hpp>
+#include<http/HttpConnection.hpp>
+
+GateServer::GateServer(boost::asio::io_context& _ioc, unsigned short port)
+          :m_ioc(_ioc)
+          , m_acceptor(_ioc, boost::asio::ip::tcp::endpoint(boost::asio::ip::address_v4::any(), port))
+{
+#ifdef _DEBUG
+          printf("[NOTICE]: server activated, listen on port %u\n", port);
+#endif
+
+          this->serverStart();
+}
+
+void GateServer::serverStart()
+{
+          std::shared_ptr<Session> session = std::make_shared<Session>(m_ioc,  this);
+
+          this->m_acceptor.async_accept(
+                    session->s_socket,
+                    std::bind(&GateServer::handleAccept, this, session, std::placeholders::_1)
+          );
+}
+
+void GateServer::handleAccept(std::shared_ptr<Session> session, boost::system::error_code ec)
+{
+          if (!ec) 
+          {
+                    /*add to session pool*/
+                    this->m_sessions.insert(std::make_pair(session->s_uuid, session));
+          
+                    /*establish HTTPConnection to handle socket*/
+                    std::shared_ptr<HTTPConnection> http(std::make_shared<HTTPConnection>(session->s_socket));
+                    http->start_service();
+          }
+          else   /*error occured!*/
+          {
+                    this->terminateSession(session->s_uuid);
+          }
+
+          /*accept connection recursively*/
+          this->serverStart();
+}
+
+void GateServer::terminateSession(const std::string& uuid)
+{
+          this->m_sessions.erase(uuid);
+}
