621f2cf3d4e8e3e3370eafe826904010d891b034
PEIHENG(Jonathan) LIU <Liupeter012001@outlook.com>
2024-07-30 11:31:50 +1000

    1.[ADD]:add signal to track control-c signal 2.[ADD]:add GateServer to handle request from port 8080

--- server/src/main.cpp
+++ server/src/main.cpp
@@ -1,7 +1,27 @@
-#include<server/gateserver.hpp>
+#include<iostream>
+#include<server/GateServer.hpp>
 
 int main() 
 {
+		  try
+		  {
+					boost::asio::io_context ioc;
+					std::shared_ptr<GateServer> server = std::make_shared<GateServer>(ioc, 8080);
+					server->serverStart();
+
+					/*setting up signal*/
+					boost::asio::signal_set signal{ ioc, SIGINT, SIGTERM };
+					signal.async_wait([&ioc](boost::system::error_code ec, int sig_number) {
+							  if (ec) {
+										return;
+							  }
+							  ioc.stop();
+					});
+					ioc.run();
+		  }
+		  catch (const std::exception& e){
+					std::cerr << e.what() << '\n';
+		  }
           return 0;
 }
 
