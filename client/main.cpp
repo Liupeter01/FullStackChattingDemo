b0246f9f0cc5bcfbc4b9ce225e58dd4e762169ca
PEIHENG(Jonathan) LIU <Liupeter012001@outlook.com>
2024-07-31 22:01:15 +1000

    1.[UPDATE]:update css loading logic

--- client/main.cpp
+++ client/main.cpp
@@ -3,11 +3,19 @@
 #include <QString>
 #include <QDebug>
 #include <QFile>
+#include <QDir>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    QFile qss(":/style/theme.qss");
+
+    QString root = QCoreApplication::applicationDirPath();
+    QString css_file = QDir::toNativeSeparators(
+        root + QDir::separator() + QString("style") + QDir::separator() + QString("theme.qss")
+    );
+
+    /*Theme css file*/
+    QFile qss(css_file);
     if(qss.open(QFile::ReadOnly)){
         qDebug() << "Open Success!\n";
         a.setStyleSheet(QString(qss.readAll()));
@@ -16,6 +24,7 @@ int main(int argc, char *argv[])
     else{
         qDebug() << "Open Failed!\n";
     }
+
     MainWindow w;
     w.show();
     return a.exec();
