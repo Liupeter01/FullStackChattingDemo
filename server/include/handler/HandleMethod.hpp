40471e24972bc52e640bf2cc406403d98bd46e7f
PEIHENG(Jonathan) LIU <Liupeter012001@outlook.com>
2024-07-31 12:28:21 +1000

    1.[MODIFY]:modify def of callback function, add callback function which has return value(currently we do not need to use it) 2.[ADD]:add json formatting handler

--- server/include/handler/HandleMethod.hpp
+++ server/include/handler/HandleMethod.hpp
@@ -13,20 +13,25 @@ class HandleMethod
           :public Singleton<HandleMethod>
 {
           friend class Singleton<HandleMethod>;
-          using  CallBackFunc = std::function<void(std::shared_ptr<HTTPConnection>)>;
+          using  CallBackNoReturn = std::function<void(std::shared_ptr<HTTPConnection>)>;
+          using CallBackWithStatus = std::function<bool(std::shared_ptr<HTTPConnection>)>;
 
 private:
           HandleMethod();
-          void registerCallBacks();
+          void registerGetCallBacks();
+          void registerPostCallBacks();
+          void jsonParsingError(std::shared_ptr<HTTPConnection> conn);
 
 public:
           ~HandleMethod();
+          void registerCallBacks();
           bool handleGetMethod(std::string str, std::shared_ptr<HTTPConnection> extended_lifetime);
+          bool handlePostMethod(std::string str, std::shared_ptr<HTTPConnection> extended_lifetime);
 
 private:
           /*CallBack Functions*/
-          std::map</*url*/std::string, CallBackFunc> get_method_callback;
-          std::map</*url*/std::string, CallBackFunc> post_method_callback;
+          std::map</*url*/std::string, CallBackNoReturn> get_method_callback;
+          std::map</*url*/std::string, CallBackWithStatus> post_method_callback;
 };
 
 #endif
--- server/src/HandleMethod.cpp
+++ server/src/HandleMethod.cpp
@@ -1,3 +1,7 @@
+#include<network/def.hpp>     //network errorcode defs
+#include<json/json.h>
+#include<json/value.h>
+#include<json/reader.h>
 #include<handler/HandleMethod.hpp>
 #include<http/HttpConnection.hpp>
 
@@ -11,7 +15,7 @@ HandleMethod::HandleMethod()
           registerCallBacks();
 }
 
-void HandleMethod::registerCallBacks()
+void HandleMethod::registerGetCallBacks()
 {
           this->get_method_callback.emplace("/get_test", [](std::shared_ptr<HTTPConnection> conn) {
                     std::size_t counter{ 0 };
@@ -22,7 +26,57 @@ void HandleMethod::registerCallBacks()
                                         << ", key = " << param.first
                                         << ", value = " << param.second << '\n';
                     }
-         });
+          });
+}
+
+void HandleMethod::registerPostCallBacks()
+{
+          this->post_method_callback.emplace("/get_verification", [this](std::shared_ptr<HTTPConnection> conn)->bool 
+          {
+                    conn->http_response.set(boost::beast::http::field::content_type, "text/json");
+                    auto body = boost::beast::buffers_to_string(conn->http_request.body().data());
+
+#ifdef _DEBUG
+                    printf("[NOTICE]: server receive post data: %s\n", body.c_str());
+#endif
+                    Json::Value send_root;        /*write into body*/
+                    Json::Value src_root;         /*store json from client*/
+                    Json::Reader reader;
+
+                    /*parsing failed*/
+                    if (!reader.parse(body, src_root)) {
+                              jsonParsingError(conn);
+                              return false;
+                    }
+
+                    if (!src_root.isMember("email")) {
+                              jsonParsingError(conn);
+                              return false;
+                    }
+
+                    send_root["error"] = static_cast<uint8_t>(ServiceStatus::SERVICE_SUCCESS);
+                    send_root["email"] = src_root["email"].asString();
+                    boost::beast::ostream(conn->http_response.body()) << send_root.toStyledString();
+                    return true;
+          });
+}
+
+void HandleMethod::jsonParsingError(std::shared_ptr<HTTPConnection> conn)
+{
+          Json::Value root;
+
+#ifdef _DEBUG
+          printf("[ERROR]: failed to parse json data!\n");
+#endif
+
+          root["error"] = static_cast<uint8_t>(ServiceStatus::JSONPARSE_ERROR);
+          boost::beast::ostream(conn->http_response.body()) << root.toStyledString();
+}
+
+void HandleMethod::registerCallBacks()
+{
+          registerGetCallBacks();
+          registerPostCallBacks();
 }
 
 bool HandleMethod::handleGetMethod(std::string str, std::shared_ptr<HTTPConnection> extended_lifetime)
@@ -32,5 +86,15 @@ bool HandleMethod::handleGetMethod(std::string str, std::shared_ptr<HTTPConnecti
                     return false;       
           }
           get_method_callback[str](extended_lifetime);
+          return true;
+}
+
+bool HandleMethod::handlePostMethod(std::string str, std::shared_ptr<HTTPConnection> extended_lifetime)
+{
+          /*Callback Func Not Found*/
+          if (post_method_callback.find(str) == post_method_callback.end()) {
+                    return false;
+          }
+          [[maybe_unused]] bool res = post_method_callback[str](extended_lifetime);
           return true;
 }
