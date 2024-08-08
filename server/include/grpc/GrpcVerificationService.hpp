89896ca9c3cd3b29850248aa2caa61d2200f476a
PEIHENG(Jonathan) LIU <Liupeter012001@outlook.com>
2024-08-08 10:52:10 +1000

    1.[REMOVE]:remove unique_ptr stub 2.[ADD]:using StubRAII class to acquire stub from pool and release it after its life cycle ends

--- server/include/grpc/GrpcVerificationService.hpp
+++ server/include/grpc/GrpcVerificationService.hpp
@@ -1,26 +1,16 @@
 #pragma once
 #ifndef GRPCVERIFICATIONSERVICE_HPP_
 #define GRPCVERIFICATIONSERVICE_HPP_
-#include<grpcpp/grpcpp.h>
-#include<message/message.grpc.pb.h>
-#include<singleton/singleton.hpp>
+#include<grpc/StubPool.hpp>
 
 class gRPCVerificationService :public Singleton< gRPCVerificationService> 
 {
           friend class Singleton< gRPCVerificationService>;
+          gRPCVerificationService() = default;
+
 public:
           ~gRPCVerificationService() = default;
-          message::GetVerificationResponse getVerificationCode(std::string email);
-
-private:
-          gRPCVerificationService();
-          gRPCVerificationService(
-                    grpc::string ip_port,
-                    std::shared_ptr<grpc::ChannelCredentials> cred = grpc::InsecureChannelCredentials()
-          );
-
-private:
-          std::unique_ptr<message::VerificationService::Stub> m_stub;
+          static message::GetVerificationResponse getVerificationCode(std::string email);
 };
 
 #endif
