6fd4ff4b4ebb96c7184be80cc7acc19010b5d893
PEIHENG(Jonathan) LIU <Liupeter012001@outlook.com>
2024-08-08 11:02:20 +1000

    1.[UPDATE]:update serverconfig class

--- server/include/config/ServerConfig.hpp
+++ server/include/config/ServerConfig.hpp
@@ -11,19 +11,56 @@ public:
           ~ServerConfig() = default;
           unsigned short GateServerPort;
           std::string VerificationServerAddress;
+          std::string MySQLAddress;
+          std::string MySQLPasswd;
+          std::string RedisAddress;
+          std::string RedisPasswd;
 
 private:
-          ServerConfig() {
-                    m_ini.load(CONFIG_HOME"config.ini");
-                    GateServerPort = m_ini["GateServer"]["port"].as<unsigned short>();
+          ServerConfig();
 
-                    VerificationServerAddress =
-                              m_ini["VerificationServer"]["host"].as<std::string>() + ':'
-                              + std::to_string(m_ini["VerificationServer"]["port"].as<unsigned short>());
-          }
+          void loadGateServerInfo();
+          void loadVerificationServerInfo();
+          void loadMySQLInfo();
+          void loadRedisInfo();
 
 private:
           ini::IniFile m_ini;
 };
+
+ServerConfig::ServerConfig()
+{
+          m_ini.load(CONFIG_HOME"config.ini");
+          loadGateServerInfo();
+          loadVerificationServerInfo();
+          loadMySQLInfo();
+          loadRedisInfo();
+}
+
+void ServerConfig::loadGateServerInfo() 
+{
+          GateServerPort = m_ini["GateServer"]["port"].as<unsigned short>();
+}
+
+void ServerConfig::loadVerificationServerInfo()
+{
+          VerificationServerAddress =
+                    m_ini["VerificationServer"]["host"].as<std::string>() + ':'
+                    + std::to_string(m_ini["VerificationServer"]["port"].as<unsigned short>());
+}
+
+void ServerConfig::loadMySQLInfo()
+{
+          // m_ini["MySQL"]["host"].as<std::string>();
+          // m_ini["MySQL"]["port"].as<unsigned short>();
+          MySQLPasswd = m_ini["MySQL"]["password"].as<std::string>();
+}
+
+void ServerConfig::loadRedisInfo()
+{
+          // m_ini["Redis"]["host"].as<std::string>();
+          // m_ini["Redis"]["port"].as<unsigned short>();
+          RedisPasswd = m_ini["Redis"]["password"].as<std::string>();
+}
 
 #endif
