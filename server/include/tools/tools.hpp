7a8d145b2d7417398052a4729671986988ae9ee5
PEIHENG(Jonathan) LIU <Liupeter012001@outlook.com>
2024-08-10 10:25:44 +1000

    1.[ADD]:add server tools, introducing smart_pointer deletor and native pointer wrapper

---
+++ server/include/tools/tools.hpp
@@ -0,0 +1,74 @@
+#pragma once
+#ifndef _TOOLS_HPP_
+#define _TOOLS_HPP_
+#include<hiredis.h>
+#include<optional>
+#include<memory>
+#include<type_traits>
+#include<string>
+
+namespace tools 
+{
+		  template<typename _Ty>
+		  class ResourcesWrapper
+		  {
+		  public:
+					ResourcesWrapper(_Ty* ctx)
+							  : m_ctx(ctx, [](_Ty* ptr) { /*do nothing*/ })
+					{}
+
+					_Ty* get() const {
+							  return m_ctx.get();
+					}
+					_Ty* operator->() const {
+							  return get();
+					}
+
+		  private:
+					std::shared_ptr<_Ty> m_ctx;
+		  };
+
+		  class RedisContextWrapper
+		  {
+		  public:
+					RedisContextWrapper(redisContext* ctx)
+							  : m_ctx(ctx, [](redisContext* ptr) { /*do nothing*/ })
+					{}
+
+					redisContext* get() const {
+							  return m_ctx.get();
+					}
+
+					operator redisContext* () const {
+							  return get();
+					}
+
+		  private:
+					std::shared_ptr< redisContext> m_ctx;
+		  };
+
+		  template<typename _Ty>
+		  struct RedisRAIIDeletor
+		  {
+					void operator()(_Ty* ptr)
+					{
+							  if (ptr == nullptr) {
+										return;
+							  }
+							  if constexpr (std::is_same_v<std::decay_t<_Ty>, redisContext>) {
+										redisFree(ptr);
+							  }
+							  else if constexpr (std::is_same_v<std::decay_t<_Ty>, redisReply>) {
+										freeReplyObject(ptr);
+							  }
+							  else {
+										delete ptr;
+							  }
+					}
+		  };
+
+		  template<typename _Ty>
+		  using RedisSmartPtr = std::unique_ptr<_Ty, RedisRAIIDeletor<_Ty>>;
+}
+
+#endif // !_TOOLS_HPP_
