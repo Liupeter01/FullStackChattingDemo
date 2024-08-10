1dd2af935f150ab222bcf28c1c972b0f6a299629
PEIHENG(Jonathan) LIU <Liupeter012001@outlook.com>
2024-08-10 21:34:25 +1000

    1.[MODIFY]:when email is not in redis DB then create a new uuid and store it

--- server/verification-server/verificationserver.js
+++ server/verification-server/verificationserver.js
@@ -4,29 +4,58 @@ const config = require('./config');
 const def = require('./defs')
 const { sendMail } = require('./email');
 const message_proto = require('./proto');
+const redis = require('./redis')
+
+async function generateEmail(email, callback){
+
+    /*generate verification code*/
+    let id = uuidv4.uuid()
+    console.log('uuid generator: ', id);
+
+    let email_text = {
+        from: config.email_username,
+        to: email,
+        subject:'verification code',
+        text: 'verification code is ' + id + ' please register within three minutes!'
+    }
+
+    /*store <key, value> to redis*/
+    let result = redis.setValueExpiracy(email, id, 180/*180s*/)
+    if(!result){
+        callback(null, 
+        {
+            email: email,
+            error: def.Status.RedisError
+        });
+    }
+
+    /*send email and wait for response*/
+    result = await sendMail(email_text);
+    console.log('send email status: ', result);
+
+    callback(null, 
+    {
+        email: email,
+        error: result ? def.Status.Success : def.Status.Exception
+    });
+}
 
 async function GetVerificationCode(GetVerificationRequest, GetVerificationResponse) {
     console.log('requested email: ', GetVerificationRequest.request.email);
     try {
-        /*generate verification code*/
-        let id = uuidv4.uuid()
-        console.log('uuid generator: ', id);
-
-        let email_text = {
-            from: config.email_username,
-            to: GetVerificationRequest.request.email,
-            subject:'verification code',
-            text: 'verification code is ' + id
+        
+        /*this email is not requested within x minutes!*/
+        let res = await redis.checkKey(GetVerificationRequest.request.email)
+        if(res == null){
+            /*generate verification code*/
+            await generateEmail(
+                GetVerificationRequest.request.email,
+                GetVerificationResponse
+            );
         }
+        else{
 
-        /*send email and wait for response*/
-        let result = await sendMail(email_text);
-        console.log('send email status: ', result);
-
-        GetVerificationResponse(null, {
-            email: GetVerificationRequest.request.email,
-            error: def.Status.Success
-        });
+        }
     } 
     catch (error) 
     {
