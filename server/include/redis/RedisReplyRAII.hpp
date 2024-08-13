c97e845013d61497fdd88213d55f1756271da8fd
Liupeter01 <Liupeter012001@outlook.com>
2024-08-13 18:44:59 +1000

    1.[FIX BUG]:solve bug on macos platform, error: member access into incomplete type 'redis::RedisContext'

--- server/include/redis/RedisReplyRAII.hpp
+++ server/include/redis/RedisReplyRAII.hpp
@@ -1,44 +1,38 @@
 #pragma once
 #ifndef _REDISREPLYRAII_HPP_
 #define _REDISREPLYRAII_HPP_
-#include<tools/tools.hpp>
+#include <redis/RedisContextRAII.hpp>
+#include <tools/tools.hpp>
 
-namespace redis
-{
-		  class RedisContext;
+namespace redis {
+class RedisReply {
+public:
+  ~RedisReply() = default;
+  RedisReply() noexcept : m_redisReply(nullptr) {}
+  /*also remove copy ctor*/
+  RedisReply(const RedisReply &) = delete;
+  RedisReply(RedisReply &&) = delete;
 
-		  class RedisReply
-		  {
-		  public:
-					~RedisReply() = default;
-					RedisReply() noexcept
-							  :m_redisReply(nullptr)
-					{}
+  RedisReply &operator=(const RedisReply &) = delete;
+  RedisReply &operator=(RedisReply &&) = delete;
 
-					/*also remove copy ctor*/
-					RedisReply(const RedisReply&) = delete;
-					RedisReply(RedisReply&&) = delete;
+  template <typename... Args>
+  bool redisCommand(RedisContext &context, const std::string &command,
+                    Args &&...args) {
+    m_redisReply.reset(reinterpret_cast<redisReply *>(
+        ::redisCommand(context.m_redisContext.get(), command.c_str(),
+                       std::forward<Args>(args)...)));
+    return checkError();
+  }
 
-					RedisReply& operator=(const RedisReply&) = delete;
-					RedisReply& operator=(RedisReply&&) = delete;
+  bool checkError();
+  std::optional<long long> getInterger() const;
+  std::optional<int> getType() const;
+  std::optional<std::string> getMessage() const;
 
-					template<typename ...Args>
-					bool redisCommand(RedisContext& context, const std::string &command, Args&&...args)
-					{
-							  m_redisReply.reset(reinterpret_cast<redisReply*>(
-										::redisCommand(context.m_redisContext.get(), command.c_str(), std::forward<Args>(args)...)
-							  ));
-							  return checkError();
-					}
-
-					bool checkError();
-					std::optional<long long>getInterger() const;
-					std::optional<int> getType() const;
-					std::optional<std::string> getMessage() const;
-
-		  private:
-					tools::RedisSmartPtr <redisReply> m_redisReply;
-		  };
-}
+private:
+  tools::RedisSmartPtr<redisReply> m_redisReply;
+};
+} // namespace redis
 
 #endif // !_REDISREPLYRAII_HPP_
