de49a83abb7e4e81381c5826186bacc09c8f73bf
PEIHENG(Jonathan) LIU <Liupeter012001@outlook.com>
2024-08-26 09:36:34 +1000

    1.[MODIFY]:modify redis pool with connectionpool template class

--- server/include/redis/RedisManager.hpp
+++ server/include/redis/RedisManager.hpp
@@ -1,52 +1,36 @@
 #pragma once
 #ifndef _REDISMANAGER_HPP_
 #define _REDISMANAGER_HPP_
-#include <condition_variable>
-#include <mutex>
-#include <queue>
+#include <spdlog/spdlog.h>
 #include <redis/RedisReplyRAII.hpp>
-#include <singleton/singleton.hpp>
+#include<service/ConnectionPool.hpp>
+#include <config/ServerConfig.hpp>
 
 namespace redis {
-namespace details {
-class RedisManager : public Singleton<RedisManager> {
-  friend class Singleton<RedisManager>;
+          class RedisConnectionPool
+                    :public connection::ConnectionPool< RedisConnectionPool, redis::RedisContext>
+          {
+                    using self = RedisConnectionPool;
+                    using context = redis::RedisContext;
+                    using context_ptr = std::unique_ptr<context>;
+                    friend class Singleton<RedisConnectionPool>;
 
-public:
-  using context = redis::RedisContext;
-  using context_ptr = std::unique_ptr<context>;
+                    RedisConnectionPool(){
+                              spdlog::info("Connecting to Redis service ip: {0}, port: {1}",
+                                        ServerConfig::get_instance()->Redis_ip_addr.c_str(),
+                                        ServerConfig::get_instance()->Redis_port);
 
-  ~RedisManager();
-  void shutdown();
-  std::optional<context_ptr> acquire();
-  void release(context_ptr stub);
+                              for (std::size_t i = 0; i < m_queue_size; ++i) {
+                                        m_stub_queue.push(std::move(std::make_unique<context>(
+                                                  ServerConfig::get_instance()->Redis_ip_addr,
+                                                  ServerConfig::get_instance()->Redis_port,
+                                                  ServerConfig::get_instance()->Redis_passwd
+                                        )));
+                              }
+                    }
 
-private:
-  RedisManager();
-  RedisManager(std::size_t poolSize, const std::string &ip, unsigned short port,
-               const std::string &password);
-
-private:
-  /*stop flag*/
-  std::atomic<bool> m_stop;
-  std::mutex m_mtx;
-  std::condition_variable m_cv;
-  std::queue<context_ptr> m_contextPool;
-};
-} // namespace details
-
-class RedisRAII {
-  using wrapper = tools::ResourcesWrapper<details::RedisManager::context>;
-
-public:
-  RedisRAII();
-  ~RedisRAII();
-  std::optional<wrapper> operator->();
-
-private:
-  bool status; // load stub success flag
-  details::RedisManager::context_ptr m_redis;
-};
-} // namespace redis
-
+          public:
+                    ~RedisConnectionPool() = default;
+          };
+}
 #endif
