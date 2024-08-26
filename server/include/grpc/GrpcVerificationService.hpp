74b29faead54531b80107170b9f90f33f065ba98
PEIHENG(Jonathan) LIU <Liupeter012001@outlook.com>
2024-08-26 10:14:51 +1000

    1.[REMOVE]:remove source file and move def to header

--- server/include/grpc/GrpcVerificationService.hpp
+++ server/include/grpc/GrpcVerificationService.hpp
@@ -1,11 +1,30 @@
 #pragma once
 #ifndef GRPCVERIFICATIONSERVICE_HPP_
 #define GRPCVERIFICATIONSERVICE_HPP_
+
+#include <network/def.hpp>
 #include <grpc/VerificationServicePool.hpp>
 
 struct gRPCVerificationService {
-  static message::GetVerificationResponse
-  getVerificationCode(std::string email);
+            static message::GetVerificationResponse getVerificationCode(std::string email) {
+                      grpc::ClientContext context;
+                      message::GetVerificationRequest request;
+                      message::GetVerificationResponse response;
+                      request.set_email(email);
+
+                      connection::ConnectionRAII<stubpool::VerificationServicePool,
+                                message::VerificationService::Stub>
+                                raii;
+
+                      grpc::Status status =
+                                raii->get()->GetVerificationCode(&context, request, &response);
+
+                      /*error occured*/
+                      if (!status.ok()) {
+                                response.set_error(static_cast<int32_t>(ServiceStatus::GRPC_ERROR));
+                      }
+                      return response;
+            }
 };
 
 #endif
