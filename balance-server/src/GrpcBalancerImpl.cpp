4597a0e82c97c1f7aa729803020b5c2c494f87ca
PEIHENG(Jonathan) LIU <Liupeter012001@outlook.com>
2024-08-27 10:45:53 +1000

    1.[ADD]:add unordermap to store info 2.[ADD]:add registernewuser token and serverbalancer functions

---
+++ balance-server/src/GrpcBalancerImpl.cpp
@@ -0,0 +1,78 @@
+#include <network/def.hpp>
+#include<grpc/GrpcBalancerImpl.hpp>
+#include <boost/uuid/uuid.hpp>
+#include <boost/uuid/uuid_generators.hpp>
+#include <boost/uuid/uuid_io.hpp>
+#include <config/ServerConfig.hpp>
+
+grpc::GrpcBalancerImpl::GrpcBalancerImpl()
+{
+          spdlog::info("Loading {} ChattingServer Configrations", ServerConfig::get_instance()->ChattingServerConfig.size());
+          for (const auto& server : ServerConfig::get_instance()->ChattingServerConfig) {
+                    spdlog::info("[Loading {}]: {}:{}", server._name, server._host, server._port);
+                    ChattingServerConfig config;
+                    config._host = server._host;
+                    config._port = server._port;
+                    config._name = server._name;
+                    config._connections = server._connections;
+                    servers[server._name] = std::move(config);
+          }
+}
+
+grpc::GrpcBalancerImpl::~GrpcBalancerImpl()
+{
+}
+
+const grpc::ChattingServerConfig &grpc::GrpcBalancerImpl::serverLoadBalancer()
+{
+          std::lock_guard<std::mutex> _lckg(server_mtx);
+
+          /*remember the lowest load server in iterator*/
+          decltype(servers)::const_iterator min = servers.begin();
+          for (auto ib = servers.begin(); ib != servers.end(); ib++) {
+                    if (ib->second._connections < min->second._connections) {
+                              min = ib;
+                    }
+          }
+          return min->second;
+}
+
+void grpc::GrpcBalancerImpl::registerUserToken(std::size_t uuid, const std::string& token)
+{
+          std::lock_guard<std::mutex> _lckg(token_mtx);
+          tokens[uuid] = token;
+}
+
+std::optional<std::string_view> grpc::GrpcBalancerImpl::getUserToken(std::size_t uuid)
+{
+          auto target = tokens.find(uuid);
+          if (target == tokens.end()) {
+                    return std::nullopt;
+          }
+          return target->second;
+}
+
+::grpc::Status grpc::GrpcBalancerImpl::addNewUserToServer(::grpc::ServerContext* context,
+                                                            const ::message::RegisterToBalancer* request, 
+                                                            ::message::GetAllocatedChattingServer* response)
+{
+          /*get the lowest load server*/
+          auto target = serverLoadBalancer();
+
+          /*generate a user token and store it into the structure first*/
+          std::string token = userTokenGenerator();
+
+          response->set_host(target._host);
+          response->set_port(target._port);
+          response->set_token(token);
+          response->set_error(static_cast<std::size_t>(ServiceStatus::SERVICE_SUCCESS));
+
+          registerUserToken(request->uuid(), token);
+          return grpc::Status::OK;
+}
+
+std::string grpc::GrpcBalancerImpl::userTokenGenerator()
+{
+          boost::uuids::uuid uuid_gen = boost::uuids::random_generator()();
+          return boost::uuids::to_string(uuid_gen);
+}
