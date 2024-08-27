3dfd23a569ab41a1a3bc4362cd07527771287b97
PEIHENG(Jonathan) LIU <Liupeter012001@outlook.com>
2024-08-27 10:43:08 +1000

    1.[UPDATE]:update serverconfig to paser variadic servers according to the server accmount parameter

--- balance-server/include/config/ServerConfig.hpp
+++ balance-server/include/config/ServerConfig.hpp
@@ -1,8 +1,17 @@
 #ifndef _INIREADER_HPP_
 #define _INIREADER_HPP_
+#include <vector>
 #include <inicpp.h>
 #include <memory>
+#include<spdlog/spdlog.h>
 #include <singleton/singleton.hpp>
+
+struct ChattingServer {
+          std::string _host;
+          std::string _port;
+          std::string _name;
+          std::size_t _connections;
+};
 
 struct ServerConfig : public Singleton<ServerConfig> {
   friend class Singleton<ServerConfig>;
@@ -11,17 +20,39 @@ public:
   ~ServerConfig() = default;
   std::string BalanceServiceAddress;
   std::string BalanceServicePort;
+  std::vector<ChattingServer>ChattingServerConfig;
 
 private:
-  ServerConfig() {
+          ServerConfig() 
+          {
+                    /*init config*/
+                    ChattingServerConfig.resize(0);
             m_ini.load(CONFIG_HOME "config.ini");
             loadBalanceServiceInfo();
-  }
+            loadChattingServiceInfo();
+          }
 
   void loadBalanceServiceInfo() {
             BalanceServiceAddress = m_ini["BalanceService"]["host"].as<std::string>();
             BalanceServicePort =
                       std::to_string(m_ini["BalanceService"]["port"].as<unsigned short>());
+  }
+
+ void loadChattingServiceInfo(){
+            std::size_t ammount = m_ini["ChattingServer"]["number"].as<int>();
+
+            /*prevent invalid number*/
+            if (static_cast<int>(ammount) <= 0) {
+                      return;
+            }
+            for (std::size_t start = 0; start < ammount; start++) {
+                      ChattingServer server;
+                      server._name = fmt::format("ChattingServer{}", std::to_string(start));
+                      server._host = m_ini[server._name]["host"].as<std::string>();
+                      server._port = m_ini[server._name]["port"].as<std::string>();
+                      server._connections = 0;
+                      ChattingServerConfig.push_back(std::move(server));
+            }
   }
 
 private:
