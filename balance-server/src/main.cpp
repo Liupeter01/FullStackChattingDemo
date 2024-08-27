b9dd97f533d23080f0cbf2d7289f040651457c50
PEIHENG(Jonathan) LIU <Liupeter012001@outlook.com>
2024-08-27 10:46:54 +1000

    1.[ADD]:add signal to control server temination 2.[ADD]:add grpc::ServerBuilder to establish a grpc server

---
+++ balance-server/src/main.cpp
@@ -0,0 +1,46 @@
+#include<thread>
+#include<boost/asio.hpp>
+#include <grpc/GrpcBalancerImpl.hpp>
+#include <config/ServerConfig.hpp>
+
+int main() {
+          std::string address = fmt::format("{}:{}", 
+                    ServerConfig::get_instance()->BalanceServiceAddress,
+                    ServerConfig::get_instance()->BalanceServicePort
+          ); 
+
+          spdlog::info("balance server start running on {}", address);
+
+          /*gRPC server*/
+          grpc::ServerBuilder builder;
+          grpc::GrpcBalancerImpl impl;
+
+          /*binding ports and establish service*/
+          builder.AddListeningPort(address, grpc::InsecureServerCredentials());
+          builder.RegisterService(&impl);
+
+          std::unique_ptr<grpc::Server> server(builder.BuildAndStart());
+
+          try {
+                    /*setting up signal*/
+                    boost::asio::io_context ioc;
+                    boost::asio::signal_set signal{ ioc, SIGINT, SIGTERM };
+                    signal.async_wait([&ioc,&server](boost::system::error_code ec, int sig_number) {
+                              if (ec) {
+                                        return;
+                              }
+                              spdlog::critical("balance server exit due to control-c input!");
+                              ioc.stop();
+                              server->Shutdown();
+                    });
+
+                    //ioc should not be blocked by server->wait()
+                    std::thread([&ioc]() {ioc.run(); }).detach();
+
+                    server->Wait();
+          }
+          catch (const std::exception& e) {
+                    std::cerr << e.what() << '\n';
+          }
+          return 0;
+}
