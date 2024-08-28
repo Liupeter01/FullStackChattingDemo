81c987efcfc3db99450a5c9729033afb319ca814
PEIHENG(Jonathan) LIU <Liupeter012001@outlook.com>
2024-08-28 15:25:51 +1000

    1.[ADD]:add connect prototype

--- client/tcpnetworkconnection.cpp
+++ client/tcpnetworkconnection.cpp
@@ -3,7 +3,11 @@
 
 TCPNetworkConnection::TCPNetworkConnection()
 {
+    /*callbacks should be registered at first(before signal)*/
+    registerCallback();
 
+    registerSocketSignal();
+    registerErrorHandling();
 }
 
 TCPNetworkConnection::~TCPNetworkConnection()
@@ -11,12 +15,43 @@ TCPNetworkConnection::~TCPNetworkConnection()
 
 }
 
-void TCPNetworkConnection::registerSignal()
+void TCPNetworkConnection::registerSocketSignal()
+{
+    /*connected to server successfully*/
+    connect(&m_socket, &QTcpSocket::connected, [this](){
+        qDebug() << "connected to server successfully";
+        emit connection_status(true);
+    });
+
+    /*server disconnected*/
+    connect(&m_socket, &QTcpSocket::disconnected, [this](){
+        qDebug() << "server disconnected";
+        emit connection_status(false);
+    });
+
+    /*receive data from server*/
+    connect(&m_socket, &QTcpSocket::readyRead, [this](){
+        QByteArray array = m_socket.readAll();
+    });
+
+}
+
+void TCPNetworkConnection::registerErrorHandling()
+{
+    connect(&m_socket, QOverload<QAbstractSocket::SocketError>::of(&QTcpSocket::errorOccurred),
+            [this]([[maybe_unused]] QTcpSocket::SocketError socketErr){
+        qDebug() << "Connection To Tcp error: " << m_socket.errorString();
+    });
+}
+
+void TCPNetworkConnection::registerCallback()
 {
 
 }
 
 void TCPNetworkConnection::establish_long_connnection(TCPNetworkConnection::ChattingServerInfo info)
 {
+    qDebug() << "Connecting to Server";
     m_server = std::move(info);
+    m_socket.connectToHost(m_server.host, m_server.port);
 }
