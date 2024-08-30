de99fbd906babe12e91f2ebae25a7edcef418138
PEIHENG(Jonathan) LIU <Liupeter012001@outlook.com>
2024-08-30 20:55:36 +1000

    1.[EMERGENCY BUG FIX]:missing ioservicepool shutdown command, causing io service pool can not be shutdown

--- gateway-server/src/main.cpp
+++ gateway-server/src/main.cpp
@@ -5,21 +5,23 @@
 
 int main() {
   try {
-    std::shared_ptr<GateServer> server = std::make_shared<GateServer>(
-        IOServicePool::get_instance()->getIOServiceContext(), // get ioc
-        ServerConfig::get_instance()->GateServerPort // get port configuration
-    );
-    server->serverStart();
-
+    auto &service_pool = IOServicePool::get_instance();
     /*setting up signal*/
     boost::asio::io_context ioc;
     boost::asio::signal_set signal{ioc, SIGINT, SIGTERM};
-    signal.async_wait([&ioc](boost::system::error_code ec, int sig_number) {
+    signal.async_wait([&ioc, &service_pool](boost::system::error_code ec, int sig_number) {
       if (ec) {
         return;
       }
+      service_pool->shutdown();
       ioc.stop();
     });
+
+    std::shared_ptr<GateServer> server = std::make_shared<GateServer>(
+              IOServicePool::get_instance()->getIOServiceContext(), // get ioc
+              ServerConfig::get_instance()->GateServerPort // get port configuration
+    );
+    server->serverStart();
     ioc.run();
   } catch (const std::exception &e) {
     std::cerr << e.what() << '\n';
