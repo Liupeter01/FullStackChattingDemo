019475e14f549f00bcb99e00dd2e053ea6b0a3a2
PEIHENG(Jonathan) LIU <Liupeter012001@outlook.com>
2024-08-31 19:35:10 +1000

    1.[ADD]:add safety io service pool

---
+++ chatting-server/src/IOServicePool.cpp
@@ -0,0 +1,47 @@
+#include <service/IOServicePool.hpp>
+
+IOServicePool::IOServicePool()
+    : IOServicePool(std::thread::hardware_concurrency() < 2
+                        ? 2
+                        : std::thread::hardware_concurrency()) {}
+
+IOServicePool::IOServicePool(std::size_t threads)
+    : m_curr(0), m_ioc_pool(threads), m_thread_pool(threads),
+      m_work_pool(threads) {
+  for (std::size_t i = 0; i < threads; ++i) {
+    /*create ioc_context guarantee io_context won't quite automatically!*/
+    m_work_pool.at(i) = std::make_unique<work>(m_ioc_pool.at(i));
+
+    /*create thread*/
+    m_thread_pool.emplace_back([this, i]() { m_ioc_pool.at(i).run(); });
+  }
+}
+
+IOServicePool::~IOServicePool() { }
+
+void IOServicePool::shutdown() {
+  for (auto &work : m_work_pool) {
+    boost::asio::io_context &ioc = work->get_io_context();
+    if (!ioc.stopped()) {
+      ioc.stop();
+    }
+
+    /*deallocate*/
+    work.reset();
+  }
+
+  /*join all the threads*/
+  for (auto &thread : m_thread_pool) {
+    if (thread.joinable()) {
+      thread.join();
+    }
+  }
+}
+
+boost::asio::io_context &IOServicePool::getIOServiceContext() {
+  boost::asio::io_context &result = m_ioc_pool.at(m_curr.fetch_add(1));
+  if (m_curr.load() == this->m_ioc_pool.size()) {
+    m_curr.store(0);
+  }
+  return result;
+}
