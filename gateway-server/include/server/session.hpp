e9be5a0931f412288c234a45e9f64b8afc4a822a
PEIHENG(Jonathan) LIU <Liupeter012001@outlook.com>
2024-09-01 20:58:50 +1000

    1.[ADD]:add stop flag for session, gateserver could deploy closesession to shutdown connection

--- gateway-server/include/server/session.hpp
+++ gateway-server/include/server/session.hpp
@@ -11,12 +11,24 @@
 template <typename _Base>
 struct Session : public std::enable_shared_from_this<Session<_Base>> {
   Session(boost::asio::io_context &_ioc, _Base *my_gate)
-      : s_socket(_ioc), s_gate(my_gate) {
+      : s_closed(false), s_socket(_ioc), s_gate(my_gate) {
     /*generate uuid string*/
     boost::uuids::uuid uuid_gen = boost::uuids::random_generator()();
     this->s_uuid = boost::uuids::to_string(uuid_gen);
   }
 
+  ~Session() { 
+            if (!s_closed) {
+                      closeSession();
+            }
+  }
+  void closeSession() {
+            /*close by accident or gateserver*/
+            s_closed = true;
+            s_socket.close();
+  }
+
+  bool s_closed;
   std::string s_uuid;
   boost::asio::ip::tcp::socket s_socket;
   _Base *s_gate;
