fdec6a95e7103f54fbc0397cb9403208a459716f
PEIHENG(Jonathan) LIU <Liupeter012001@outlook.com>
2024-09-02 18:25:27 +1000

    1.[ADD]:support signal SIGTERM and SIGINT in order to support server exit 2.[ADD]:execute AsyncServer main code

---
+++ chatting-server/src/main.cpp
@@ -0,0 +1,35 @@
+#include <spdlog/spdlog.h>
+#include <server/AsyncServer.hpp>
+#include <service/IOServicePool.hpp>
+#include <config/ServerConfig.hpp>
+
+int main() {
+          try {
+                    auto & service_pool = IOServicePool::get_instance();
+
+                    /*setting up signal*/
+                    boost::asio::io_context ioc;
+                    boost::asio::signal_set signal{ ioc, SIGINT, SIGTERM };
+                    signal.async_wait([&ioc, &service_pool](boost::system::error_code ec, int sig_number) {
+                              if (ec) {
+                                        return;
+                              }
+                              ioc.stop();
+                              service_pool->shutdown();
+                    });
+                     
+                    /*create chatting server*/
+                    std::shared_ptr<AsyncServer> server = std::make_shared<AsyncServer>(
+                              service_pool->getIOServiceContext(), 
+                              ServerConfig::get_instance()->ChattingServerPort
+                    );
+
+                    server->startAccept();
+                    /**/
+                    ioc.run();
+          }
+          catch (const std::exception& e) {
+                    spdlog::error("{}", e.what());
+          }
+          return 0;
+}
