aebbeaabf29af58303ff89cb1449cabd972cdb07
PEIHENG(Jonathan) LIU <Liupeter012001@outlook.com>
2024-09-03 10:59:41 +1000

    1.[ADD]:add recvinfo to store message info, because m_buffer is going to be deleted

--- client/tcpnetworkconnection.h
+++ client/tcpnetworkconnection.h
@@ -8,7 +8,7 @@
 #include <QTcpSocket>
 #include <QUrl>
 #include <functional>
-#include <messagenode.hpp>
+#include <MsgNode.hpp>
 #include <singleton.hpp>
 
 class TCPNetworkConnection
@@ -16,67 +16,73 @@ class TCPNetworkConnection
       public Singleton<TCPNetworkConnection>,
       public std::enable_shared_from_this<TCPNetworkConnection> {
 
-  Q_OBJECT
-  friend class Singleton<TCPNetworkConnection>;
-  using Callbackfunction = std::function<void(QJsonObject &&)>;
+    Q_OBJECT
+    friend class Singleton<TCPNetworkConnection>;
+    using Callbackfunction = std::function<void(QJsonObject &&)>;
 
 public:
-  struct ChattingServerInfo {
-    // init
-    ChattingServerInfo() : uuid(), host(), token(), port(0) {}
+    struct ChattingServerInfo {
+        // init
+        ChattingServerInfo() : uuid(), host(), token(), port(0) {}
 
-    QString uuid = "";
-    QString host = "";
-    std::size_t port = 0;
-    QString token = "";
-  };
+        QString uuid = "";
+        QString host = "";
+        std::size_t port = 0;
+        QString token = "";
+    };
 
 public:
-  ~TCPNetworkConnection();
+    ~TCPNetworkConnection();
 
-  /*use signal to trigger data sending*/
-  void send_data(SendNode<QByteArray> &&data);
-
-private:
-  TCPNetworkConnection();
-
-  void registerNetworkEvent();
-  void registerSocketSignal();
-  void registerCallback();
-  void registerErrorHandling();
+    /*use signal to trigger data sending*/
+    void send_data(SendNode<QByteArray> &&data);
 
 private:
-  bool checkJsonForm(const QJsonObject &json);
+    TCPNetworkConnection();
+
+    void registerNetworkEvent();
+    void registerSocketSignal();
+    void registerCallback();
+    void registerErrorHandling();
+
+private:
+    bool checkJsonForm(const QJsonObject &json);
 
 private slots:
-  void slot_establish_long_connnection(
-      TCPNetworkConnection::ChattingServerInfo info);
+    void slot_establish_long_connnection(
+        TCPNetworkConnection::ChattingServerInfo info);
 
 signals:
-  void signal_establish_long_connnection(
-      TCPNetworkConnection::ChattingServerInfo info);
+    void signal_establish_long_connnection(
+        TCPNetworkConnection::ChattingServerInfo info);
 
-  /*return connection status to login class*/
-  void signal_connection_status(bool status);
+    /*return connection status to login class*/
+    void signal_connection_status(bool status);
 
-  /*login to server failed*/
-  void signal_login_failed(ServiceStatus status);
+    /*login to server failed*/
+    void signal_login_failed(ServiceStatus status);
 
-  /*if login success, then switch to chatting dialog*/
-  void signal_switch_chatting_dialog();
+    /*if login success, then switch to chatting dialog*/
+    void signal_switch_chatting_dialog();
 
 private:
-  /*establish tcp socket with server*/
-  QTcpSocket m_socket;
+    /*establish tcp socket with server*/
+    QTcpSocket m_socket;
 
-  /*save server connection info*/
-  ChattingServerInfo m_server;
+    /*save server connection info*/
+    ChattingServerInfo m_server;
 
-  /*create a connection buffer to store the data transfer from server*/
-  RecvNode<QByteArray> m_buffer;
+    struct RecvInfo{
+        uint16_t _id = 0;
+        uint16_t _length = 0;
+        QByteArray _msg;
+    } m_received;
 
-  /*according to service type to execute callback*/
-  std::map<ServiceType, Callbackfunction> m_callbacks;
+    /*create a connection buffer to store the data transfer from server*/
+    RecvNode<QByteArray> m_buffer;
+
+    /*according to service type to execute callback*/
+    std::map<ServiceType, Callbackfunction> m_callbacks;
 };
 
 #endif // TCPNETWORKCONNECTION_H
