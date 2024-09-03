f876d49f0e32a58887194903f5565c108840dee5
Liupeter01 <Liupeter012001@outlook.com>
2024-09-03 11:26:04 +1000

    1.[FORMATTING]

--- chatting-server/include/handler/SyncLogic.hpp
+++ chatting-server/include/handler/SyncLogic.hpp
@@ -34,10 +34,8 @@ private:
   void processing();
   void registerCallbacks();
   void execute(pair &&node);
-  void SyncLogic::generateErrorMessage(const std::string& msg,
-            ServiceType type,
-            ServiceStatus status,
-            SessionPtr conn);
+  void SyncLogic::generateErrorMessage(const std::string &msg, ServiceType type,
+                                       ServiceStatus status, SessionPtr conn);
 
   /*Execute Operations*/
   void handlingLogin(ServiceType srv_type, std::shared_ptr<Session> session,
