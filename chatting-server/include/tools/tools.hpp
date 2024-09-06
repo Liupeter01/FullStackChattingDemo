18c5c5979310ef73bda1ec621bd409807133d241
PEIHENG(Jonathan) LIU <Liupeter012001@outlook.com>
2024-09-06 13:33:37 +1000

    1.[ADD]:add a charconv header to the project 2.[ADD]:using std::from_chars to convert string to integer/float

--- chatting-server/include/tools/tools.hpp
+++ chatting-server/include/tools/tools.hpp
@@ -2,6 +2,9 @@
 #ifndef _TOOLS_HPP_
 #define _TOOLS_HPP_
 #include <memory>
+#include <optional>
+#include <string_view>
+#include <charconv>
 
 namespace tools {
 template <typename _Ty> class ResourcesWrapper {
@@ -14,6 +17,15 @@ public:
 private:
   std::shared_ptr<_Ty> m_ctx;
 };
+
+template<typename _Ty>
+std::optional<_Ty> string_to_value(std::string_view value) {
+          _Ty _temp_res{};
+          std::from_chars_result res = std::from_chars(value.data(), value.data() + value.size(), _temp_res);
+          if (res.ec == std::errc() && res.ptr == value.data() + value.size())  return _temp_res;
+          return std::nullopt;
+}
+
 } // namespace tools
 
 #endif // !_TOOLS_HPP_
