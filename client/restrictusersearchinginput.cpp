0aa0dc2d04605c2e8ca37acacfc87c6b5b88e964
PEIHENG(Jonathan) LIU <Liupeter012001@outlook.com>
2024-09-10 09:52:13 +1000

    1.[ADD]:add restrict class which inherited from qlineedit to constraints the input size of username

---
+++ client/restrictusersearchinginput.cpp
@@ -0,0 +1,40 @@
+#include "restrictusersearchinginput.h"
+
+RestrictUserSearchingInput::RestrictUserSearchingInput(QWidget *parent)
+    :RestrictUserSearchingInput(0, parent)
+{}
+
+RestrictUserSearchingInput::RestrictUserSearchingInput(std::size_t max_length, QWidget *parent)
+    : m_maxLen(max_length)
+    , QLineEdit(parent)
+{
+    /*when lineedit text changed, then limit text length*/
+    registerTextChangedSignal();
+}
+
+RestrictUserSearchingInput::~RestrictUserSearchingInput(){}
+
+void RestrictUserSearchingInput::setMaxLength(std::size_t max_length)
+{
+    m_maxLen = max_length;
+}
+
+void RestrictUserSearchingInput::constraintTextLength(const QString &text)
+{
+    if(m_maxLen <= 0){
+        return;
+    }
+    QByteArray arr = text.toUtf8();
+
+    /*the size should not exceed m_maxLen*/
+    if(arr.size() > m_maxLen){
+        qDebug() << "Input Exceeded! Length = " << arr.size();
+        setText(QString::fromUtf8(arr.left(m_maxLen)));
+    }
+}
+
+void RestrictUserSearchingInput::registerTextChangedSignal()
+{
+    connect(this, &QLineEdit::textChanged, this, &RestrictUserSearchingInput::constraintTextLength);
+}
+
---
+++ client/restrictusersearchinginput.h
@@ -0,0 +1,28 @@
+#ifndef RESTRICTUSERSEARCHINGINPUT_H
+#define RESTRICTUSERSEARCHINGINPUT_H
+
+#include <QLineEdit>
+
+class RestrictUserSearchingInput : public QLineEdit
+{
+    Q_OBJECT
+
+public:
+    RestrictUserSearchingInput(QWidget *parent = nullptr);
+    RestrictUserSearchingInput(std::size_t max_length, QWidget *parent = nullptr);
+    ~RestrictUserSearchingInput();
+    void setMaxLength(std::size_t max_length);
+
+protected:
+    void constraintTextLength(const QString &text);
+
+private:
+    /*when lineedit text changed, then limit text length*/
+    void registerTextChangedSignal();
+
+private:
+    /*constraints the max input length of username*/
+    std::size_t m_maxLen;
+};
+
+#endif // RESTRICTUSERSEARCHINGINPUT_H
