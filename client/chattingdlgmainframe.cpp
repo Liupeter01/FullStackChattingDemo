24cc3850baddeaf269c55fddf7e5861711b83b0f
PEIHENG(Jonathan) LIU <Liupeter012001@outlook.com>
2024-09-10 10:25:48 +1000

    1.[MODIFY]:modify the max width of the ui 2.[ADD]:add search username action and signal 3.[ADD]:promote pushbutton to buttondisplayswitching in order to support QICON switching 4.[ADD]:promote lineedit to restrictusersearch to constraint user's input

--- client/chattingdlgmainframe.cpp
+++ client/chattingdlgmainframe.cpp
@@ -1,53 +1,128 @@
 #include "chattingdlgmainframe.h"
-#include "tools.h"
 #include "ui_chattingdlgmainframe.h"
 #include <QFile>
+#include "tools.h"
+#include <QAction>
 
 ChattingDlgMainFrame::ChattingDlgMainFrame(QWidget *parent)
-    : QDialog(parent), ui(new Ui::ChattingDlgMainFrame) {
-  ui->setupUi(this);
+    : QDialog(parent)
+    , ui(new Ui::ChattingDlgMainFrame)
+{
+    ui->setupUi(this);
 
-  /*register signal for ui display*/
-  registerSignal();
+    /*register signal for ui display*/
+    registerSignal();
 
-  /*load qicon for chatting main frame*/
-  Tools::loadIconResources({"add_friend_normal.png", "add_friend_hover.png",
-                            "add_friend_clicked.png"});
+     /*register Qaction for search edit ui widget*/
+    registerSearchEditAction();
 
-  /*set default button icon*/
-  Tools::setPushButtonIcon(ui->search_user_button, "add_friend_normal.png");
+    /*register search edit signal*/
+    registerSearchEditSignal();
+
+    /*constraint the length of username when client try to search*/
+    ui->search_user_edit->setMaxLength(20);
+
+    /*load qicon for chatting main frame*/
+    Tools::loadIconResources({
+        "add_friend_normal.png",
+        "add_friend_hover.png",
+        "add_friend_clicked.png"
+    });
+
+    /*set default button icon*/
+    Tools::setPushButtonIcon(ui->search_user_button,
+                             "add_friend_normal.png");
 }
 
 void ChattingDlgMainFrame::registerSignal() {
-  connect(ui->search_user_button, &ButtonDisplaySwitching::clicked, this,
-          &ChattingDlgMainFrame::updateSearchUserButton);
-  connect(ui->search_user_button, &ButtonDisplaySwitching::update_display, this,
-          &ChattingDlgMainFrame::updateSearchUserButton);
+    connect(ui->search_user_button,
+            &ButtonDisplaySwitching::clicked, this,
+            &ChattingDlgMainFrame::updateSearchUserButton
+            );
+    connect(ui->search_user_button,
+            &ButtonDisplaySwitching::update_display, this,
+            &ChattingDlgMainFrame::updateSearchUserButton
+            );
 }
 
-void ChattingDlgMainFrame::updateSearchUserButton() {
-  auto state = ui->search_user_button->getState();
-  /*if it is selected, then it gets the highest proity*/
-  if (state.select == PushButtonState::SelectedStatus::ENABLED) {
-    setCursor(Qt::PointingHandCursor);
-    Tools::setPushButtonIcon(ui->search_user_button, "add_friend_clicked.png");
-  } else {
-    /*currently, its not selected! switch to hover
-     *if it is not hovered! then switch to normal
-     */
-    Tools::setPushButtonIcon(ui->search_user_button,
+void ChattingDlgMainFrame::registerSearchEditAction()
+{
+    /*add a search icon*/
+    m_searchAction = new QAction(ui->search_user_edit);
+    m_searchAction->setIcon(Tools::loadIcon(QT_DEMO_HOME "/res/search.png").value());
 
-                             state.hover ==
-                                     PushButtonState::HoverStatus::DISABLED
-                                 ? "add_friend_normal.png"
-                                 : "add_friend_hover.png");
+    /*put it on the front position of line edit*/
+    ui->search_user_edit->addAction(m_searchAction, QLineEdit::LeadingPosition);
+    ui->search_user_edit->setPlaceholderText(QString("Searching"));
 
-    if (state.hover == PushButtonState::HoverStatus::ENABLED) {
-      setCursor(Qt::PointingHandCursor);
-    } else {
-      unsetCursor();
+    /*add a transparent cancel button*/
+    m_cancelAction = new QAction(ui->search_user_edit);
+    m_cancelAction->setIcon(Tools::loadIcon(QT_DEMO_HOME "/res/close_transparent.png").value());
+
+    /*put it on the back position of line edit*/
+    ui->search_user_edit->addAction(m_cancelAction, QLineEdit::TrailingPosition);
+
+    //connect(ui->search_user_edit, )
+}
+
+void ChattingDlgMainFrame::registerSearchEditSignal()
+{
+    /*when user input sth, then change transparent icon to visible icon*/
+    connect(ui->search_user_edit, &QLineEdit::textChanged, this, [this](const QString &str){
+        m_cancelAction->setIcon(Tools::loadIcon(str.isEmpty()?
+            QT_DEMO_HOME "/res/close_transparent.png"
+            :QT_DEMO_HOME "/res/close_search.png").value());
+    });
+
+    /*when user trigger cancel button, then clear all the text*/
+    connect(m_cancelAction, &QAction::triggered, [this](){
+        /*clear username search text*/
+        ui->search_user_edit->clear();
+
+        /*switch to transparent icon, because there is no input*/
+        m_cancelAction->setIcon(Tools::loadIcon(QT_DEMO_HOME "/res/close_transparent.png").value());
+
+        /**/
+        ui->search_user_edit->clearFocus();
+    });
+}
+
+void ChattingDlgMainFrame::updateSearchUserButton()
+{
+    auto state = ui->search_user_button->getState();
+    /*if it is selected, then it gets the highest proity*/
+    if (state.select == PushButtonState::SelectedStatus::ENABLED) {
+        setCursor(Qt::PointingHandCursor);
+        Tools::setPushButtonIcon(
+            ui->search_user_button,
+            "add_friend_clicked.png"
+            );
     }
-  }
+    else
+    {
+        /*currently, its not selected! switch to hover
+         *if it is not hovered! then switch to normal
+         */
+        Tools::setPushButtonIcon(
+            ui->search_user_button,
+
+            state.hover == PushButtonState::HoverStatus::DISABLED ?
+                "add_friend_normal.png" : "add_friend_hover.png"
+            );
+
+        if(state.hover == PushButtonState::HoverStatus::ENABLED){
+            setCursor(Qt::PointingHandCursor);
+        }
+        else{
+            unsetCursor();
+        }
+
+    }
 }
 
-ChattingDlgMainFrame::~ChattingDlgMainFrame() { delete ui; }
+ChattingDlgMainFrame::~ChattingDlgMainFrame()
+{
+    delete m_searchAction;
+    delete m_cancelAction;
+    delete ui;
+}
--- client/chattingdlgmainframe.h
+++ client/chattingdlgmainframe.h
@@ -1,28 +1,38 @@
 #ifndef CHATTINGDLGMAINFRAME_H
 #define CHATTINGDLGMAINFRAME_H
 
-#include <QDialog>
-#include <QIcon>
 #include <map>
+#include <QIcon>
 #include <optional>
+#include <QDialog>
 
 namespace Ui {
 class ChattingDlgMainFrame;
 }
 
-class ChattingDlgMainFrame : public QDialog {
-  Q_OBJECT
+class ChattingDlgMainFrame : public QDialog
+{
+    Q_OBJECT
 
 public:
-  explicit ChattingDlgMainFrame(QWidget *parent = nullptr);
-  ~ChattingDlgMainFrame();
+    explicit ChattingDlgMainFrame(QWidget *parent = nullptr);
+    ~ChattingDlgMainFrame();
 
 private:
-  void registerSignal();
-  void updateSearchUserButton();
+    void registerSignal();
+
+    /*register action for search edit ui item*/
+    void registerSearchEditAction();
+    void registerSearchEditSignal();
+    void updateSearchUserButton();
 
 private:
-  Ui::ChattingDlgMainFrame *ui;
+    /*reserve for search line edit*/
+    QAction* m_searchAction;
+
+    /*reserve for cancel user searching*/
+    QAction* m_cancelAction;
+    Ui::ChattingDlgMainFrame *ui;
 };
 
 #endif // CHATTINGDLGMAINFRAME_H
