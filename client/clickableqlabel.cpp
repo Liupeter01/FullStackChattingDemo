ea27cacf797978706a407354686b4d9a0b77c062
PEIHENG(Jonathan) LIU <Liupeter012001@outlook.com>
2024-09-11 11:54:24 +1000

    1.[REMOVE]:remove passworddisplayswitiching because it is not genertic 2.[ADD]:add and emit update_display signal to interface class, all ui handle the signal by themselves

---
+++ client/clickableqlabel.cpp
@@ -0,0 +1,41 @@
+#include "clickableqlabel.h"
+#include <QDebug>
+#include <QMouseEvent>
+
+ClickableQLabel::ClickableQLabel(QWidget *parent,
+                                                   Qt::WindowFlags f)
+    : m_state(), QLabel(parent) {}
+
+ClickableQLabel::~ClickableQLabel() {}
+
+const LabelState &ClickableQLabel::getState() const { return m_state; }
+
+void ClickableQLabel::mousePressEvent(QMouseEvent *event) {
+  if (event->button() == Qt::LeftButton) {
+    qDebug() << QString("left button clicked!");
+
+    /*switch visiable status after each mouse press event*/
+    this->m_state.visiable =
+        (!this->m_state.visiable ? LabelState::VisiableStatus::ENABLED
+                                 : LabelState::VisiableStatus::DISABLED);
+
+    emit clicked();
+  }
+  QLabel::mousePressEvent(event);
+}
+
+/*mouse enter selected section*/
+void ClickableQLabel::enterEvent(QEnterEvent *event) {
+  qDebug() << QString("mouse entered!");
+  this->m_state.hover = LabelState::HoverStatus::ENABLED;
+  emit update_display();
+  QLabel::enterEvent(event);
+}
+
+/*mouse leave*/
+void ClickableQLabel::leaveEvent(QEvent *event) {
+  qDebug() << QString("mouse leave!");
+  this->m_state.hover = LabelState::HoverStatus::DISABLED;
+  emit update_display();
+  QLabel::leaveEvent(event);
+}
---
+++ client/clickableqlabel.h
@@ -0,0 +1,37 @@
+#ifndef CLICKABLEQLABEL_H
+#define CLICKABLEQLABEL_H
+#include "tools.h"
+#include <QLabel>
+#include <QWidget>
+#include <Qt>
+
+//ClickableQLabel
+//clickableqlabel
+class ClickableQLabel : public QLabel {
+  Q_OBJECT
+
+public:
+  ClickableQLabel(QWidget *parent = nullptr,
+                           Qt::WindowFlags f = Qt::WindowFlags());
+  ~ClickableQLabel();
+
+  const LabelState &getState() const;
+
+signals:
+  void update_display();
+  void clicked();
+
+protected:
+  virtual void mousePressEvent(QMouseEvent *event) override;
+
+  /*mouse enter selected section*/
+  virtual void enterEvent(QEnterEvent *event) override;
+
+  /*mouse leave*/
+  virtual void leaveEvent(QEvent *event) override;
+
+private:
+  LabelState m_state;
+};
+
+#endif // ClickableQLabel_H
