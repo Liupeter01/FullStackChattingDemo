d86b4b2534388d53ef593079a954f64913177b24
PEIHENG(Jonathan) LIU <Liupeter012001@outlook.com>
2024-09-17 09:22:20 +1000

    1.[TEST]:test both recv and send functions

--- client/chattingstackpage.cpp
+++ client/chattingstackpage.cpp
@@ -12,31 +12,48 @@ ChattingStackPage::ChattingStackPage(QWidget *parent)
 ChattingStackPage::~ChattingStackPage() { delete ui; }
 
 void ChattingStackPage::on_send_message_clicked() {
-  ChattingRole role = ChattingRole::Sender;
-  QString username = "test_username";
-  QString iconpath = QT_DEMO_HOME "/res/header.png";
+  //ChattingRole role = ChattingRole::Sender;
+  QString recv_name = "Bob";
+  QString send_name = "Alex";
+  QString recv_icon = QT_DEMO_HOME "/res/recv.png";
+  QString send_icon = QT_DEMO_HOME "/res/send.png";
 
   const QVector<MsgInfo> &list = ui->user_input->getMsgList();
 
+
+
   for (std::size_t index = 0; index < list.size(); ++index) {
     /*currently, we are the msssage sender*/
-    QWidget *bubble{nullptr};
-    ChattingMsgItem *item = new ChattingMsgItem(role);
+    QWidget *bubble_send{nullptr}, *bubble_recv{nullptr};
+
+    /*create this for send*/
+    ChattingMsgItem *item_sender = new ChattingMsgItem(ChattingRole::Sender);
+
+    /*create this for recv*/
+    ChattingMsgItem *item_recv = new ChattingMsgItem(ChattingRole::Receiver);
+
     MsgInfo info = list[index];
 
-    item->setupUserName(username);
-    item->setupIconPixmap(QPixmap(iconpath));
+    item_sender->setupUserName(send_name);
+    item_sender->setupIconPixmap(QPixmap(send_icon));
+
+    item_recv->setupUserName(recv_name);
+    item_recv->setupIconPixmap(QPixmap(recv_icon));
 
     if (info.type == MsgType::TEXT) {
-      bubble = new TextMsgBubble(role, info.content);
+      bubble_send = new TextMsgBubble(ChattingRole::Sender, info.content);
     } else if (info.type == MsgType::IMAGE) {
-      bubble = new PictureMsgBubble(role, info.pixmap);
+      bubble_send = new PictureMsgBubble(ChattingRole::Sender, info.pixmap);
     } else if (info.type == MsgType::FILE) {
     }
 
-    if (bubble != nullptr) {
-      item->setupBubbleWidget(bubble);
-      ui->chatting_record->pushBackItem(item);
+    bubble_recv = new TextMsgBubble(ChattingRole::Receiver, QString("recv success!"));
+
+    if (bubble_send != nullptr && bubble_recv != nullptr) {
+      item_sender->setupBubbleWidget(bubble_send);
+      item_recv->setupBubbleWidget(bubble_recv);
+      ui->chatting_record->pushBackItem(item_sender);
+      ui->chatting_record->pushBackItem(item_recv);
     }
   }
 }
