5a7a7e79a702332584ff3244e7738a88aaf0fab7
Liupeter01 <Liupeter012001@outlook.com>
2024-09-16 19:48:40 +1000

    1.[FORMATTING]

--- client/picturemsgbubble.cpp
+++ client/picturemsgbubble.cpp
@@ -1,31 +1,27 @@
 #include "picturemsgbubble.h"
 
+PictureMsgBubble::PictureMsgBubble(ChattingRole role, const QPixmap &picture,
+                                   QWidget *parent)
+    : m_picture(new QLabel), m_picSize(PICTURE_WIDTH, PICTURE_HEIGHT),
+      MsgBubbleBase(role, parent) {
+  /*setup bubble widget for base class*/
+  this->setupBubbleWidget(m_picture);
 
-PictureMsgBubble::PictureMsgBubble(ChattingRole role,const QPixmap &picture, QWidget *parent)
-    :m_picture(new QLabel)
-    , m_picSize(PICTURE_WIDTH, PICTURE_HEIGHT)
-    , MsgBubbleBase(role, parent)
-{
-    /*setup bubble widget for base class*/
-    this->setupBubbleWidget(m_picture);
-
-    /*fix picture size*/
-    this->setupPixmap(picture);
+  /*fix picture size*/
+  this->setupPixmap(picture);
 }
 
-PictureMsgBubble::~PictureMsgBubble()
-{
-}
+PictureMsgBubble::~PictureMsgBubble() {}
 
-void PictureMsgBubble::setupPixmap(const QPixmap &pic)
-{
-    QPixmap fixed = pic.scaled(m_picSize, Qt::KeepAspectRatio);
-    m_picture->setScaledContents(true);
-    m_picture->setPixmap(fixed);
+void PictureMsgBubble::setupPixmap(const QPixmap &pic) {
+  QPixmap fixed = pic.scaled(m_picSize, Qt::KeepAspectRatio);
+  m_picture->setScaledContents(true);
+  m_picture->setPixmap(fixed);
 
-    auto left_margin = this->layout()->contentsMargins().left();
-    auto right_margin = this->layout()->contentsMargins().right();
-    auto up_down_margin = this->layout()->contentsMargins().bottom();
+  auto left_margin = this->layout()->contentsMargins().left();
+  auto right_margin = this->layout()->contentsMargins().right();
+  auto up_down_margin = this->layout()->contentsMargins().bottom();
 
-    m_picture->setMaximumSize(fixed.width() + left_margin + right_margin, fixed.height() + 2 * up_down_margin);
+  m_picture->setMaximumSize(fixed.width() + left_margin + right_margin,
+                            fixed.height() + 2 * up_down_margin);
 }
--- client/textmsgbubble.h
+++ client/textmsgbubble.h
@@ -1,38 +1,38 @@
 #ifndef TEXTMSGBUBBLE_H
 #define TEXTMSGBUBBLE_H
 
+#include "msgbubblebase.h"
 #include <QFont>
 #include <QTextEdit>
-#include "msgbubblebase.h"
 
-class TextMsgBubble : public MsgBubbleBase
-{
-    Q_OBJECT
+class TextMsgBubble : public MsgBubbleBase {
+  Q_OBJECT
 public:
-    TextMsgBubble(ChattingRole role, const QString &text, QWidget *parent = nullptr);
-    ~TextMsgBubble();
+  TextMsgBubble(ChattingRole role, const QString &text,
+                QWidget *parent = nullptr);
+  ~TextMsgBubble();
 
-    /* set text for textedit
-    * we have to calculate the max width for the textedit
-    */
-    void setupTextMsg(const QString &str);
+  /* set text for textedit
+   * we have to calculate the max width for the textedit
+   */
+  void setupTextMsg(const QString &str);
 
 protected:
-    bool eventFilter(QObject *object, QEvent *event) override;
+  bool eventFilter(QObject *object, QEvent *event) override;
 
 private:
-    /*if window size changed then adjust bubble size*/
-    void adjustBubbleSize();
+  /*if window size changed then adjust bubble size*/
+  void adjustBubbleSize();
 
-    /*add style sheet*/
-    void addStyleSheet();
+  /*add style sheet*/
+  void addStyleSheet();
 
-    /*using a parameter to adjust height for user experience*/
-    static constexpr std::size_t COMPENSATION_VALUE = 30;
+  /*using a parameter to adjust height for user experience*/
+  static constexpr std::size_t COMPENSATION_VALUE = 30;
 
 private:
-    QFont m_font;
-    QTextEdit* m_text;
+  QFont m_font;
+  QTextEdit *m_text;
 };
 
 #endif // TEXTMSGBUBBLE_H
