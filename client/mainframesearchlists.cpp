f1b4ba6ee4f04a407d548e9bf79d854e760f579f
PEIHENG(Jonathan) LIU <Liupeter012001@outlook.com>
2024-09-20 10:51:27 +1000

    1.[ADD]:inhertied from mainframeshowlists 2.[ADD]:this class should retrieve network data from TCPNetwork class 3.[ADD]:add qss for listwidget style

---
+++ client/mainframesearchlists.cpp
@@ -0,0 +1,51 @@
+#include <QListWidgetItem>
+#include "addnewuserwidget.h"
+#include "mainframesearchlists.h"
+
+MainFrameSearchLists::MainFrameSearchLists(QWidget *parent)
+    :MainFrameShowLists(parent)
+{
+    /*regsiter signal slot*/
+    registerSignal();
+
+    /*add a startup widget inside the list*/
+    addNewUserWidget();
+
+    /*add style sheet for search_list and add user widget*/
+    addStyleSheet();
+}
+
+MainFrameSearchLists::~MainFrameSearchLists()
+{
+}
+
+void MainFrameSearchLists::registerSignal()
+{
+    /*when user click searched item, the emit itemClicked signal*/
+    connect(this, &QListWidget::itemClicked, this, &MainFrameSearchLists::slot_item_clicked);
+}
+
+void MainFrameSearchLists::addNewUserWidget()
+{
+    AddNewUserWidget*new_inserted(new AddNewUserWidget());
+    QListWidgetItem *item(new QListWidgetItem);
+    item->setSizeHint(new_inserted->sizeHint());
+
+    this->addItem(item);
+    this->setItemWidget(item, new_inserted);
+    this->update();
+}
+
+void MainFrameSearchLists::addStyleSheet()
+{
+    this->setStyleSheet("#search_list{border:none;outline:none}");
+    this->setStyleSheet("#search_list::item::selected{background-color:#d3d7d4;border:none;outline:none}");
+    this->setStyleSheet("#search_list::item::hover{background-color:rgb(206,207,208);border:none;outline:none}");
+    this->setStyleSheet("#search_list::focus{black;border:none;outline:none}");
+}
+
+void MainFrameSearchLists::slot_item_clicked(QListWidgetItem *clicked_item)
+{
+    qDebug() << "item clicked! ";
+}
+
---
+++ client/mainframesearchlists.h
@@ -0,0 +1,34 @@
+#ifndef MAINFRAMESEARCHLISTS_H
+#define MAINFRAMESEARCHLISTS_H
+
+#include "mainframeshowlists.h"
+
+//forward declearation
+class QListWidgetItem;
+
+class MainFrameSearchLists : public MainFrameShowLists
+{
+    Q_OBJECT
+
+public:
+    MainFrameSearchLists(QWidget *parent = nullptr);
+    ~MainFrameSearchLists();
+
+private:
+    void registerSignal();
+
+    /*add a startup widget inside the list*/
+    void addNewUserWidget();
+
+    /*add style sheet*/
+    void addStyleSheet();
+
+private slots:
+    /*user click the item shown in the ListWidget*/
+    void slot_item_clicked(QListWidgetItem *clicked_item);
+
+private:
+
+};
+
+#endif // MAINFRAMESEARCHLISTS_H
