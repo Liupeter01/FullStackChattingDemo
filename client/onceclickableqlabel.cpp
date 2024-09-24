db7158b24bc01c9de85e849e194f4c1d981b0a85
PEIHENG(Jonathan) LIU <Liupeter012001@outlook.com>
2024-09-24 11:14:06 +1000

    1.[ADD]:inherited from QLabel and add click event 2.[ADD]:only support one time click

---
+++ client/onceclickableqlabel.cpp
@@ -0,0 +1,25 @@
+#include "onceclickableqlabel.h"
+#include <QDebug>
+#include <QMouseEvent>
+
+OnceClickableQLabel::OnceClickableQLabel(QWidget *parent)
+{
+    this->setCursor(Qt::PointingHandCursor);
+}
+
+OnceClickableQLabel::~OnceClickableQLabel()
+{
+}
+
+void OnceClickableQLabel::mousePressEvent(QMouseEvent *event)
+{
+    if (event->button() == Qt::LeftButton) {
+        qDebug() << QString("left button clicked!");
+        emit clicked(this->text());
+        //return;
+    }
+    QLabel::mousePressEvent(event);
+}
+
+
+
---
+++ client/onceclickableqlabel.h
@@ -0,0 +1,21 @@
+#ifndef ONCECLICKABLEQLABEL_H
+#define ONCECLICKABLEQLABEL_H
+
+#include <QLabel>
+
+class OnceClickableQLabel : public QLabel
+{
+    Q_OBJECT
+
+public:
+    OnceClickableQLabel(QWidget* parent = nullptr);
+    ~OnceClickableQLabel();
+
+signals:
+    void clicked(QString);
+
+protected:
+    virtual void mousePressEvent(QMouseEvent *event) override;
+};
+
+#endif // ONCECLICKABLEQLABEL_H
