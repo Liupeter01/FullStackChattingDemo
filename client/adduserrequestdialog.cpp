3fd42ae0c10fbf8df32a60ae804a61514f5a1c62
Liupeter01 <Liupeter012001@outlook.com>
2024-09-26 09:15:46 +1000

    1.[FORMATTING]

--- client/adduserrequestdialog.cpp
+++ client/adduserrequestdialog.cpp
@@ -54,10 +54,10 @@ void AddUserRequestDialog::registerSignal() {
 
 void AddUserRequestDialog::setupDefaultInfo() {
 
-    /**limit user input ammount to 21**/
-    ui->nick_name_edit->setMaxLength(21);
-    ui->request_msg_edit->setMaxLength(21);
-    ui->tag_input->setMaxLength(21);
+  /**limit user input ammount to 21**/
+  ui->nick_name_edit->setMaxLength(21);
+  ui->request_msg_edit->setMaxLength(21);
+  ui->tag_input->setMaxLength(21);
 
   /*input label
    * 1. setup start pos
@@ -65,7 +65,6 @@ void AddUserRequestDialog::setupDefaultInfo() {
    */
   ui->tag_input->move(2, 2);
   ui->tag_input->setFixedHeight(36);
-
 
   /*hide status display bar*/
   ui->user_tag_display_bar->hide();
