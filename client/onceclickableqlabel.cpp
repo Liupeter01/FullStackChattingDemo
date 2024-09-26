d7a2be378e62df01727dee3806ef0fd1b466b66d
Liupeter01 <Liupeter012001@outlook.com>
2024-09-26 14:02:33 +1000

    1.[ADD]:add set status function to allow user set the status bt themselves

--- client/onceclickableqlabel.cpp
+++ client/onceclickableqlabel.cpp
@@ -8,11 +8,21 @@ OnceClickableQLabel::OnceClickableQLabel(QWidget *parent) {
 
 OnceClickableQLabel::~OnceClickableQLabel() {}
 
+void OnceClickableQLabel::setCurrentState(LabelState::VisiableStatus status) {
+  m_state.visiable = status;
+  emit update_display(this->text(), m_state);
+}
+
 void OnceClickableQLabel::mousePressEvent(QMouseEvent *event) {
   if (event->button() == Qt::LeftButton) {
     qDebug() << QString("left button clicked!");
-    emit clicked(this->text());
-    // return;
+
+    /*switch visiable status after each mouse press event*/
+    this->m_state.visiable =
+        (!this->m_state.visiable ? LabelState::VisiableStatus::ENABLED
+                                 : LabelState::VisiableStatus::DISABLED);
+
+    emit clicked(this->text(), m_state);
   }
   QLabel::mousePressEvent(event);
 }
--- client/onceclickableqlabel.h
+++ client/onceclickableqlabel.h
@@ -1,6 +1,6 @@
 #ifndef ONCECLICKABLEQLABEL_H
 #define ONCECLICKABLEQLABEL_H
-
+#include "tools.h"
 #include <QLabel>
 
 class OnceClickableQLabel : public QLabel {
@@ -10,11 +10,18 @@ public:
   OnceClickableQLabel(QWidget *parent = nullptr);
   ~OnceClickableQLabel();
 
+public:
+  void setCurrentState(LabelState::VisiableStatus status);
+
 signals:
-  void clicked(QString);
+  void clicked(QString, LabelState);
+  void update_display(QString, LabelState);
 
 protected:
   virtual void mousePressEvent(QMouseEvent *event) override;
+
+private:
+  LabelState m_state;
 };
 
 #endif // ONCECLICKABLEQLABEL_H
