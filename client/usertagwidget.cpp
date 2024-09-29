70350725f7a2422a13b71568cc28afa762a6d4ec
Liupeter01 <Liupeter012001@outlook.com>
2024-09-29 10:42:17 +1000

    1.[FORMATTING]

--- client/usertagwidget.cpp
+++ client/usertagwidget.cpp
@@ -1,79 +1,65 @@
-#include <QFontMetricsF>
 #include "usertagwidget.h"
 #include "ui_usertagwidget.h"
+#include <QFontMetricsF>
 
 UserTagWidget::UserTagWidget(QWidget *parent)
-    : QWidget(parent)
-    , ui(new Ui::UserTagWidget)
-    , m_tagSize(width(), height())
-{
-    ui->setupUi(this);
+    : QWidget(parent), ui(new Ui::UserTagWidget), m_tagSize(width(), height()) {
+  ui->setupUi(this);
 
-    /*load image for unselected*/
-    loadImage();
+  /*load image for unselected*/
+  loadImage();
 
-    /*style sheet*/
-    addStyleSheet();
+  /*style sheet*/
+  addStyleSheet();
 }
 
-UserTagWidget::~UserTagWidget()
-{
-    delete ui;
+UserTagWidget::~UserTagWidget() { delete ui; }
+
+void UserTagWidget::setTagName(const QString &text) {
+  ui->tag_label->setText(text);
+  ui->tag_label->adjustSize();
+
+  /*update tag size according to label input*/
+  updateTagSize();
 }
 
-void UserTagWidget::setTagName(const QString &text)
-{
-    ui->tag_label->setText(text);
-    ui->tag_label->adjustSize();
+QSize UserTagWidget::sizeHint() const { return m_tagSize; }
 
-    /*update tag size according to label input*/
-    updateTagSize();
+const QSize UserTagWidget::getImageSize() {
+  return QSize(image_width, image_height);
 }
 
-QSize UserTagWidget::sizeHint() const
-{
-    return m_tagSize;
+void UserTagWidget::slot_close(QString str, LabelState state) {
+  /*emit signal to the outside*/
+  emit signal_close(str, state);
 }
 
-const QSize UserTagWidget::getImageSize()
-{
-    return QSize(image_width, image_height);
+void UserTagWidget::loadImage() {
+  Tools::setQLableImage(ui->unselected_click, "unselect_tag.png");
 }
 
-void UserTagWidget::slot_close(QString str, LabelState state)
-{
-    /*emit signal to the outside*/
-    emit signal_close(str, state);
+void UserTagWidget::addStyleSheet() {
+  ui->widget->setStyleSheet(QString(
+      "#UserTagWidget "
+      "QWidget{background-color:#daf6e7;color:#48bf56;border-radius:10px;}"));
 }
 
-void UserTagWidget::loadImage()
-{
-    Tools::setQLableImage(ui->unselected_click, "unselect_tag.png");
+void UserTagWidget::registerSignal() {
+  connect(ui->unselected_click, &OnceClickableQLabel::clicked, this,
+          &UserTagWidget::slot_close);
 }
 
-void UserTagWidget::addStyleSheet()
-{
-    ui->widget->setStyleSheet(QString("#UserTagWidget QWidget{background-color:#daf6e7;color:#48bf56;border-radius:10px;}"));
-}
+void UserTagWidget::updateTagSize() {
+  QFontMetricsF fontMetrics(ui->tag_label->font());
 
-void UserTagWidget::registerSignal()
-{
-    connect(ui->unselected_click, &OnceClickableQLabel::clicked, this, &UserTagWidget::slot_close);
-}
+  auto text_width = fontMetrics.horizontalAdvance(ui->tag_label->text());
+  auto text_height = fontMetrics.height();
 
-void UserTagWidget::updateTagSize()
-{
-    QFontMetricsF fontMetrics(ui->tag_label->font());
+  qDebug() << "text_width = " << text_width << "\ntext_height = " << text_height
+           << "\n";
 
-    auto text_width = fontMetrics.horizontalAdvance(ui->tag_label->text());
-    auto text_height = fontMetrics.height();
+  this->setFixedWidth(text_width + image_width + COMPENSATION_WIDTH);
+  this->setFixedHeight(text_height + COMPENSATION_HEIGHT);
 
-    qDebug() << "text_width = " << text_width
-             << "\ntext_height = " << text_height
-             <<"\n";
-
-    this->setFixedWidth(text_width + image_width + COMPENSATION_WIDTH);
-    this->setFixedHeight(text_height + COMPENSATION_HEIGHT);
-
-    m_tagSize = QSize(this->width(), this->height());
+  m_tagSize = QSize(this->width(), this->height());
 }
--- client/usertagwidget.h
+++ client/usertagwidget.h
@@ -1,63 +1,61 @@
 #ifndef USERTAGWIDGET_H
 #define USERTAGWIDGET_H
 
+#include "tools.h"
 #include <QSize>
 #include <QWidget>
-#include "tools.h"
 
 namespace Ui {
 class UserTagWidget;
 }
 
-class UserTagWidget : public QWidget
-{
-    Q_OBJECT
+class UserTagWidget : public QWidget {
+  Q_OBJECT
 
 public:
-    explicit UserTagWidget(QWidget *parent = nullptr);
-    ~UserTagWidget();
+  explicit UserTagWidget(QWidget *parent = nullptr);
+  ~UserTagWidget();
 
 public:
-    /*load image parameters*/
-    static const QSize getImageSize();
+  /*load image parameters*/
+  static const QSize getImageSize();
 
-    /*user set tag info*/
-    void setTagName(const QString &text);
+  /*user set tag info*/
+  void setTagName(const QString &text);
 
-    /*when request dialog loading user tag widget*/
-    virtual QSize sizeHint() const;
+  /*when request dialog loading user tag widget*/
+  virtual QSize sizeHint() const;
 
 signals:
-    void signal_close(QString str, LabelState state);
+  void signal_close(QString str, LabelState state);
 
 public slots:
-    void slot_close(QString str, LabelState state);
+  void slot_close(QString str, LabelState state);
 
 private:
+  /*using a parameter to adjust height for user experience*/
+  static constexpr std::size_t COMPENSATION_HEIGHT = 2;
+  static constexpr std::size_t COMPENSATION_WIDTH = 4;
 
-    /*using a parameter to adjust height for user experience*/
-    static constexpr std::size_t COMPENSATION_HEIGHT = 2;
-    static constexpr std::size_t COMPENSATION_WIDTH = 4;
+  /*reserve for image*/
+  static constexpr std::size_t image_width = 15;
+  static constexpr std::size_t image_height = 15;
 
-    /*reserve for image*/
-    static constexpr std::size_t image_width = 15;
-    static constexpr std::size_t image_height = 15;
+  /*add image*/
+  void loadImage();
 
-    /*add image*/
-    void loadImage();
+  /*style sheet*/
+  void addStyleSheet();
 
-    /*style sheet*/
-    void addStyleSheet();
+  /*signal<->slot*/
+  void registerSignal();
 
-    /*signal<->slot*/
-    void registerSignal();
-
-    /*update tag size by considering font size*/
-    void updateTagSize();
+  /*update tag size by considering font size*/
+  void updateTagSize();
 
 private:
-    Ui::UserTagWidget *ui;
-    QSize m_tagSize;
+  Ui::UserTagWidget *ui;
+  QSize m_tagSize;
 };
 
 #endif // USERTAGWIDGET_H
