b5086ac797cc7b30f3b534bb031cc8179e7c00e9
PEIHENG(Jonathan) LIU <Liupeter012001@outlook.com>
2024-10-08 10:16:43 +1100

    1.[ADD]:add image loading path

--- client/UserNameCard.h
+++ client/UserNameCard.h
@@ -7,11 +7,12 @@ enum class Sex { Male, Female };
 
 /*record the critical info of the user*/
 struct UserNameCard {
-  UserNameCard(std::size_t uid, QString nickname, QString desc, Sex sex)
-      : m_sex(sex), m_uid(uid), m_nickname(nickname), m_description(desc) {}
+  UserNameCard(std::size_t uid, QString avator_path, QString nickname, QString desc, Sex sex)
+      : m_sex(sex), m_uid(uid), m_nickname(nickname), m_description(desc), m_avatorPath(avator_path) {}
 
   Sex m_sex;
   std::size_t m_uid;
+  QString m_avatorPath;
   QString m_nickname;
   QString m_description;
 };
