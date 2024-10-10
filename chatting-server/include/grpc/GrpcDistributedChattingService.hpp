078b7fd9df3819cee41e0d04e61321d643fa4c84
Liupeter01 <Liupeter012001@outlook.com>
2024-10-10 11:09:08 +1100

    1.[FORMATTING]

--- chatting-server/include/grpc/GrpcDistributedChattingService.hpp
+++ chatting-server/include/grpc/GrpcDistributedChattingService.hpp
@@ -1,25 +1,25 @@
 #pragma once
 #ifndef _GRPCDISTRIBUTEDCHATTINGSERVICE_HPP_
 #define _GRPCDISTRIBUTEDCHATTINGSERVICE_HPP_
-#include <unordered_map>
-#include <network/def.hpp>
 #include <grpc/BalanceServicePool.hpp>
 #include <grpc/DistributedChattingServicePool.hpp>
+#include <network/def.hpp>
+#include <unordered_map>
 
 class gRPCDistributedChattingService
-          : public Singleton<gRPCDistributedChattingService> 
-{
-          friend class Singleton<gRPCDistributedChattingService>;
-          gRPCDistributedChattingService();
+    : public Singleton<gRPCDistributedChattingService> {
+  friend class Singleton<gRPCDistributedChattingService>;
+  gRPCDistributedChattingService();
 
 public:
-          virtual ~gRPCDistributedChattingService() = default;
+  virtual ~gRPCDistributedChattingService() = default;
 
 private:
-          std::unordered_map<
-                    /*server_name*/ std::string,
-                    /*DistributedChattingServicePool*/std::unique_ptr< stubpool::DistributedChattingServicePool>
-          > m_pools;
+  std::unordered_map<
+      /*server_name*/ std::string,
+      /*DistributedChattingServicePool*/ std::unique_ptr<
+          stubpool::DistributedChattingServicePool>>
+      m_pools;
 };
 
 #endif //_GRPCDISTRIBUTEDCHATTINGSERVICE_HPP_
--- chatting-server/src/GrpcDistributedChattingService.cpp
+++ chatting-server/src/GrpcDistributedChattingService.cpp
@@ -1,25 +1,35 @@
 #include <grpc/GrpcBalanceService.hpp>
 #include <grpc/GrpcDistributedChattingService.hpp>
 
-gRPCDistributedChattingService::gRPCDistributedChattingService()
-{
-          /*pass current server name as a parameter to the balance server, and returns all peers*/
-          auto response = gRPCBalancerService::getPeerServerLists(ServerConfig::get_instance()->ChattingServerName);
+gRPCDistributedChattingService::gRPCDistributedChattingService() {
+  /*pass current server name as a parameter to the balance server, and returns
+   * all peers*/
+  auto response = gRPCBalancerService::getPeerServerLists(
+      ServerConfig::get_instance()->ChattingServerName);
 
-          if (response.error() !=
-                    static_cast<int32_t>(ServiceStatus::SERVICE_SUCCESS)) {
-                    spdlog::error("[Balance Server] try retrieve peer servers' info failed!, error code {}", response.error());
-                    std::abort();
-          }
+  if (response.error() !=
+      static_cast<int32_t>(ServiceStatus::SERVICE_SUCCESS)) {
+    spdlog::error("[Balance Server] try retrieve peer servers' info failed!, "
+                  "error code {}",
+                  response.error());
+    std::abort();
+  }
 
-          /*get server lists*/
-          auto& peer_servers = response.lists();
+  /*get server lists*/
+  auto &peer_servers = response.lists();
 
-          /*traversal server lists and create multiple DistributedChattingServicePool according to host and port*/
-          std::for_each(peer_servers.begin(), peer_servers.end(), [this](const message::ChattingPeerServer& server) {
-                    m_pools.insert(std::pair<std::string, std::unique_ptr< stubpool::DistributedChattingServicePool>>(
-                              /*peer server's name*/ server.name(),
-                              /*create a pool*/std::make_unique<stubpool::DistributedChattingServicePool>(server.host(), server.port())
-                    ));
-          });
+  /*traversal server lists and create multiple DistributedChattingServicePool
+   * according to host and port*/
+  std::for_each(
+      peer_servers.begin(), peer_servers.end(),
+      [this](const message::ChattingPeerServer &server) {
+        m_pools.insert(
+            std::pair<
+                std::string,
+                std::unique_ptr<stubpool::DistributedChattingServicePool>>(
+                /*peer server's name*/ server.name(),
+                /*create a pool*/ std::make_unique<
+                    stubpool::DistributedChattingServicePool>(server.host(),
+                                                              server.port())));
+      });
 }
