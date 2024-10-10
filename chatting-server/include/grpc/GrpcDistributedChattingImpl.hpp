829276b55cfa47cc5f4ccb8075f1fe75b997ba22
PEIHENG(Jonathan) LIU <Liupeter012001@outlook.com>
2024-10-10 11:08:55 +1100

    1.[ADD]:override message::DistributedChattingService::Service functions 2.[ADD]:add def

---
+++ chatting-server/include/grpc/GrpcDistributedChattingImpl.hpp
@@ -0,0 +1,33 @@
+#pragma once
+#ifndef _GRPCDISRIBUTEDCHATTINGIMPL_
+#define _GRPCDISRIBUTEDCHATTINGIMPL_
+#include <grpcpp/grpcpp.h>
+#include <message/message.grpc.pb.h>
+#include <mutex>
+#include <network/def.hpp>
+#include <optional>
+#include <string_view>
+#include <unordered_map>
+
+namespace grpc {
+          class GrpcDistributedChattingImpl final :public message::DistributedChattingService::Service {
+          public:
+                    GrpcDistributedChattingImpl();
+                    virtual ~GrpcDistributedChattingImpl();
+
+          public:
+                    // A send friend request message to another user B
+                    virtual ::grpc::Status SendFriendRequest(::grpc::ServerContext* context, const ::message::AddNewFriendRequest* request, ::message::AddNewFriendResponse* response);
+                    // User B agreed with user A's friend adding request
+                    virtual ::grpc::Status ConfirmFriendRequest(::grpc::ServerContext* context, const ::message::AuthoriseRequest* request, ::message::AuthoriseResponse* response);
+                    // Verify that B is still A's friend:
+                    virtual ::grpc::Status FriendshipVerification(::grpc::ServerContext* context, const ::message::AuthoriseRequest* request, ::message::AuthoriseResponse* response);
+                    // transfer chatting message from user A to B
+                    virtual ::grpc::Status NormalChattingMsg(::grpc::ServerContext* context, const ::message::SendChattingMsgRequest* request, ::message::SendChattingMsgResponse* response);
+
+          private:
+
+          };
+}
+
+#endif //_GRPCDISRIBUTEDCHATTINGIMPL_
---
+++ chatting-server/src/GrpcDistributedChattingImpl.cpp
@@ -0,0 +1,49 @@
+#include <grpc/GrpcDistributedChattingImpl.hpp>
+
+grpc::GrpcDistributedChattingImpl::GrpcDistributedChattingImpl() {}
+
+grpc::GrpcDistributedChattingImpl::~GrpcDistributedChattingImpl() {}
+
+// A send friend request message to another user B
+::grpc::Status 
+grpc::GrpcDistributedChattingImpl::SendFriendRequest(
+          ::grpc::ServerContext* context, 
+          const ::message::AddNewFriendRequest* request, 
+          ::message::AddNewFriendResponse* response)
+{
+
+          return grpc::Status::OK;
+}
+
+// User B agreed with user A's friend adding request
+::grpc::Status 
+grpc::GrpcDistributedChattingImpl::ConfirmFriendRequest(
+          ::grpc::ServerContext* context, 
+          const ::message::AuthoriseRequest* request, 
+          ::message::AuthoriseResponse* response) 
+{
+
+          return grpc::Status::OK;
+}
+
+// Verify that B is still A's friend:
+::grpc::Status 
+grpc::GrpcDistributedChattingImpl::FriendshipVerification(
+          ::grpc::ServerContext* context, 
+          const ::message::AuthoriseRequest* request, 
+          ::message::AuthoriseResponse* response) 
+{
+
+          return grpc::Status::OK;
+}
+
+// transfer chatting message from user A to B
+::grpc::Status 
+grpc::GrpcDistributedChattingImpl::NormalChattingMsg(
+          ::grpc::ServerContext* context, 
+          const ::message::SendChattingMsgRequest* request, 
+          ::message::SendChattingMsgResponse* response)
+{
+
+          return grpc::Status::OK;
+}
