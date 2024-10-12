4870a392a1e6fa9654b6c7bd35b15dc4a32f9770
PEIHENG(Jonathan) LIU <Liupeter012001@outlook.com>
2024-10-12 19:00:08 +1100

    1.[UPDATE]:add template for redis

--- chatting-server/include/tools/tools.hpp
+++ chatting-server/include/tools/tools.hpp
@@ -2,9 +2,11 @@
 #ifndef _TOOLS_HPP_
 #define _TOOLS_HPP_
 #include <charconv>
+#include <hiredis.h>
 #include <memory>
 #include <optional>
-#include <string_view>
+#include <string>
+#include <type_traits>
 
 namespace tools {
 template <typename _Ty> class ResourcesWrapper {
@@ -17,6 +19,39 @@ public:
 private:
   std::shared_ptr<_Ty> m_ctx;
 };
+
+class RedisContextWrapper {
+public:
+          RedisContextWrapper(redisContext* ctx)
+                    : m_ctx(ctx, [](redisContext* ptr) { /*do nothing*/ }) {}
+
+          redisContext* get() const { return m_ctx.get(); }
+
+          operator redisContext* () const { return get(); }
+
+private:
+          std::shared_ptr<redisContext> m_ctx;
+};
+
+template <typename _Ty> struct RedisRAIIDeletor {
+          void operator()(_Ty* ptr) {
+                    if (ptr == nullptr) {
+                              return;
+                    }
+                    if constexpr (std::is_same_v<std::decay_t<_Ty>, redisContext>) {
+                              redisFree(ptr);
+                    }
+                    else if constexpr (std::is_same_v<std::decay_t<_Ty>, redisReply>) {
+                              freeReplyObject(ptr);
+                    }
+                    else {
+                              delete ptr;
+                    }
+          }
+};
+
+template <typename _Ty>
+using RedisSmartPtr = std::unique_ptr<_Ty, RedisRAIIDeletor<_Ty>>;
 
 template <typename _Ty>
 std::optional<_Ty> string_to_value(std::string_view value) {
