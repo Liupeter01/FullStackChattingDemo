136017ee38f090ad79b6389c0a47d0b37e9d2133
PEIHENG(Jonathan) LIU <Liupeter012001@outlook.com>
2024-10-13 10:25:09 +1100

    1.[ADD]:add a user basic info def header file

---
+++ chatting-server/include/server/UserNameCard.hpp
@@ -0,0 +1,23 @@
+#ifndef USERNAMECARD_H
+#define USERNAMECARD_H
+#include <string>
+#include <memory>
+#include <cstdlib>
+
+enum class Sex { Male, Female };
+
+/*record the critical info of the user*/
+struct UserNameCard {
+  UserNameCard(std::size_t uid, const std::string &avator_path, const std::string& nickname,
+            const std::string& desc, Sex sex)
+      : m_sex(sex), m_uuid(uid), m_nickname(nickname), m_description(desc),
+        m_avatorPath(avator_path) {}
+
+  Sex m_sex;
+  std::size_t m_uuid;
+  std::string m_avatorPath;
+  std::string m_nickname;
+  std::string m_description;
+};
+
+#endif // USERNAMECARD_H
