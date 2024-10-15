d4272c7c850af064e344f5216e2f3b76e0bf7cfa
PEIHENG(Jonathan) LIU <Liupeter012001@outlook.com>
2024-10-15 11:07:33 +1100

    1.[MODIFY]:modify size_t uuid to qstring uuid

--- client/UserNameCard.h
+++ client/UserNameCard.h
@@ -8,13 +8,13 @@ enum class Sex { Male, Female };
 
 /*record the critical info of the user*/
 struct UserNameCard {
-  UserNameCard(std::size_t uid, QString avator_path, QString nickname,
-               QString desc, Sex sex)
-      : m_sex(sex), m_uid(uid), m_nickname(nickname), m_description(desc),
+  UserNameCard(const QString & uuid, const QString & avator_path, const QString & nickname,
+               const QString & desc, Sex sex)
+      : m_sex(sex), m_uuid(uuid), m_nickname(nickname), m_description(desc),
         m_avatorPath(avator_path) {}
 
   Sex m_sex;
-  std::size_t m_uid;
+  QString m_uuid;
   QString m_avatorPath;
   QString m_nickname;
   QString m_description;
