732b07fdea3730517e508257b6ae1b50470ae56f
PEIHENG(Jonathan) LIU <Liupeter012001@outlook.com>
2024-10-17 11:44:57 +1100

    1.[MODIFY]:modify uuid from size_t to QString

--- client/useraccountmanager.hpp
+++ client/useraccountmanager.hpp
@@ -12,20 +12,19 @@ public:
   void set_host(const QString &_host) { m_info.host = _host; }
   void set_port(const QString &_port) { m_info.port = _port; }
   void set_token(const QString &_token) { m_info.token = _token; }
-  void set_uuid(const std::size_t _uuid) { m_info.uuid = _uuid; }
+  void set_uuid(const QString & _uuid) { m_info.uuid = _uuid; }
 
   const QString &get_host() const { return m_info.host; }
   const QString &get_port() const { return m_info.port; }
   const QString &get_token() const { return m_info.token; }
-  const std::size_t get_uuid() const { return m_info.uuid; }
+  const QString get_uuid() const { return m_info.uuid; }
 
 private:
   UserAccountManager() : m_info() {}
 
 private:
   struct ChattingServerInfo {
-    ChattingServerInfo() : uuid(0) {}
-    std::size_t uuid;
+    QString uuid;
     QString host;
     QString port;
     QString token;
