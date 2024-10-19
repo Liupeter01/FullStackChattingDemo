67e92a08b0cef07dba670d7110289fb9c99aa180
PEIHENG(Jonathan) LIU <Liupeter012001@outlook.com>
2024-10-19 21:17:44 +1100

    1.[ADD]:add username info

--- chatting-server/include/server/UserNameCard.hpp
+++ chatting-server/include/server/UserNameCard.hpp
@@ -9,13 +9,14 @@ enum class Sex { Male, Female };
 /*record the critical info of the user*/
 struct UserNameCard {
   UserNameCard(const std::string &uuid, const std::string &avator_path,
-               const std::string &nickname, const std::string &desc, Sex sex)
-      : m_sex(sex), m_uuid(uuid), m_nickname(nickname), m_description(desc),
+            const std::string& username, const std::string &nickname, const std::string &desc, Sex sex)
+      : m_sex(sex), m_uuid(uuid), m_username(username), m_nickname(nickname), m_description(desc),
         m_avatorPath(avator_path) {}
 
   Sex m_sex;
   std::string m_uuid;
   std::string m_avatorPath;
+  std::string m_username;
   std::string m_nickname;
   std::string m_description;
 };
--- client/UserNameCard.h
+++ client/UserNameCard.h
@@ -8,14 +8,15 @@ enum class Sex { Male, Female };
 
 /*record the critical info of the user*/
 struct UserNameCard {
-  UserNameCard(const QString & uuid, const QString & avator_path, const QString & nickname,
+  UserNameCard(const QString & uuid, const QString & avator_path, const QString & username, const QString & nickname,
                const QString & desc, Sex sex)
-      : m_sex(sex), m_uuid(uuid), m_nickname(nickname), m_description(desc),
+      : m_sex(sex), m_uuid(uuid), m_username(username), m_nickname(nickname), m_description(desc),
         m_avatorPath(avator_path) {}
 
   Sex m_sex;
   QString m_uuid;
   QString m_avatorPath;
+  QString m_username;
   QString m_nickname;
   QString m_description;
 };
