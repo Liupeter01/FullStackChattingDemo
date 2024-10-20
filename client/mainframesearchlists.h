00168bb750622466b24d7db9a861dc7e92ee7daa
PEIHENG(Jonathan) LIU <Liupeter012001@outlook.com>
2024-10-20 19:49:41 +1100

    1.[ADD]:add search username handling method 2.[ADD]:add waiting termination signal on ChattingDlgmainframe 3.[ADD]:create different AddUserNameCardDialog according to signal emitted by TcpNetworkManager

--- client/mainframesearchlists.h
+++ client/mainframesearchlists.h
@@ -1,6 +1,8 @@
 #ifndef MAINFRAMESEARCHLISTS_H
 #define MAINFRAMESEARCHLISTS_H
 
+#include "def.hpp"
+#include <optional>
 #include <memory>
 #include "addusernamecarddialog.h"
 #include "mainframeshowlists.h"
@@ -15,12 +17,30 @@ public:
   MainFrameSearchLists(QWidget *parent = nullptr);
   virtual ~MainFrameSearchLists();
 
+signals:
+  /*
+    * waiting for data from remote server
+    * status = true: activate
+    * status = false: deactivate
+    */
+   void signal_waiting_for_data(bool status);
+
+private slots:
+    void slot_search_username(std::optional<std::shared_ptr<UserNameCard>> info, ServiceStatus status);
+
 private:
+  /*register signal*/
+  void registerSignal();
+
   /*add a startup widget inside the list*/
   void addNewUserWidget();
 
   /*add style sheet*/
   void addStyleSheet();
+
+private:
+  /*close status dialog*/
+  std::shared_ptr<QDialog> m_Dlg;
 };
 
 #endif // MAINFRAMESEARCHLISTS_H
