2265c31e73ccbc522cd53ce6a6b04a2960477a6d
Liupeter01 <Liupeter012001@outlook.com>
2024-10-22 19:07:21 +1100

    1.[REMOVE]:remove server test assets 2.[REMOVE]:remove /get_test GET method and keep the basic structure inside the code

--- gateway-server/src/HandleMethod.cpp
+++ gateway-server/src/HandleMethod.cpp
@@ -16,18 +16,7 @@ HandleMethod::HandleMethod() {
   registerCallBacks();
 }
 
-void HandleMethod::registerGetCallBacks() {
-  this->get_method_callback.emplace(
-      "/get_test", [](std::shared_ptr<HTTPConnection> conn) {
-        std::size_t counter{0};
-        boost::beast::ostream(conn->http_response.body()) << "get_test";
-        for (const auto &param : conn->http_params) {
-          boost::beast::ostream(conn->http_response.body())
-              << "index = " << ++counter << ", key = " << param.first
-              << ", value = " << param.second << '\n';
-        }
-      });
-}
+void HandleMethod::registerGetCallBacks() {}
 
 void HandleMethod::registerPostCallBacks() {
   this->post_method_callback.emplace(
