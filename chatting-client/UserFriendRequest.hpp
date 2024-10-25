b40128ed02b7b139d04fd4d01b08d47e56b29e6b
PEIHENG(Jonathan) LIU <Liupeter012001@outlook.com>
2024-10-25 21:51:59 +1100

    1.[UPDATE]:inhertied UserNameCard 2.[MODIFY]:remove serveral class's members

--- chatting-client/UserFriendRequest.hpp
+++ chatting-client/UserFriendRequest.hpp
@@ -1,16 +1,24 @@
 #ifndef USERFRIENDREQUEST_HPP
 #define USERFRIENDREQUEST_HPP
 
+#include <UserNameCard.h>
 #include <memory>
 #include <QString>
 
-struct UserFriendRequest{
-    UserFriendRequest(const QString &from, const QString &to, const QString &nick, const QString &msg)
-        :src_uuid(from), dst_uuid(to), nickname(nick), message(msg)
+struct UserFriendRequest : public UserNameCard{
+    UserFriendRequest(const QString &from,
+                      const QString &to,
+                      const QString &nick,
+                      const QString &msg,
+                      const QString &avator_path,
+                      const QString &username,
+                      const QString &desc,
+                      Sex sex)
+        : UserNameCard(from, avator_path, username, nick, desc, sex)
+        , dst_uuid(to)
+        , message(msg)
     {}
-    QString src_uuid;
     QString dst_uuid;
-    QString nickname;
     QString message;
 };
 
