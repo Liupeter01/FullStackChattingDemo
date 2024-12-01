e679ea6a0e7264a36a39a04abc62d4226723b041
PEIHENG(Jonathan) LIU <Liupeter012001@outlook.com>
2024-12-01 12:15:55 +1100

    1.[ADD]:add friend request creating, user auth and agree with friend request, create auth friend request, get auth friend or get friending request list Queries

--- chatting-server/include/sql/MySQLConnection.hpp
+++ chatting-server/include/sql/MySQLConnection.hpp
@@ -6,12 +6,14 @@
 #include <boost/mysql/tcp_ssl.hpp>
 #include <chrono>
 #include <map>
+#include <vector>
 #include <optional>
 #include <server/UserNameCard.hpp>
 #include <string_view>
 
 /*delcaration*/
 struct UserNameCard;
+struct UserFriendRequest;
 
 struct MySQLRequestStruct {
   std::string_view m_username;
@@ -26,14 +28,18 @@ enum class MySQLSelection : uint8_t {
   HEART_BEAT,
   FIND_EXISTING_USER,
   CREATE_NEW_USER,    // register new user
-  ACQUIRE_NEW_UID,    // get uid for user
-  UPDATE_UID_COUNTER, // add up to uid accounter
   UPDATE_USER_PASSWD, // update user password
   USER_LOGIN_CHECK,   // check login username & password
   USER_UUID_CHECK,    // check account uuid in DB
   USER_PROFILE,       // check account user profile
   GET_USER_UUID,      // get uuid by username
-  USER_FRIEND_REQUEST // User A send friend request to B
+
+  CREATE_FRIENDING_REQUEST, // User A send friend request to B, status = 0
+  UPDATE_FRIEND_REQUEST_STATUS,   //User A agreed with B's request, then change status = 1
+  CREATE_AUTH_FRIEND_ENTRY, //After update status, add user A & B's info to AuthFriend Table
+
+  GET_FRIEND_REQUEST_LIST, //Get User's Request List
+  GET_AUTH_FRIEND_LIST,       //Get User's Auth Friend List
 };
 
 class MySQLConnection {
@@ -57,6 +63,21 @@ public:
   bool createFriendRequest(const std::size_t src_uuid,
                            const std::size_t dst_uuid,
                            std::string_view nickname, std::string_view message);
+
+  /*update user friend request to confirmed status*/
+  bool updateFriendingStatus(const std::size_t src_uuid,
+            const std::size_t dst_uuid);
+
+  bool createAuthFriendsRelation(const std::size_t src_uuid,
+            const std::size_t dst_uuid, const std::string& alternative);
+
+  /*
+  * get specfic amount of friending request
+  * which is restricted by a startpos and an interval
+  * the valid data is from [starpos, startpos + interval - 1]
+  */
+  std::optional<std::vector<std::unique_ptr<UserFriendRequest>>>
+            getFriendingRequestList(const std::size_t dst_uuid, const std::size_t start_pos, const std::size_t interval);
 
   /*insert new user, call MySQLSelection::CREATE_NEW_USER*/
   bool registerNewUser(MySQLRequestStruct &&request);
