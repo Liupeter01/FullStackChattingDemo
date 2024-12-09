6d2ee784326604f94486a0b1cd5f3fd935ee8b6d
PEIHENG(Jonathan) LIU <Liupeter012001@outlook.com>
2024-12-09 11:23:04 +1100

    1.[REPLACE]:replace usernamecard with Friendchattinghistory 2.[ADD]:add :getChattingContext func

--- chatting-client/chattinghistorywidget.h
+++ chatting-client/chattinghistorywidget.h
@@ -1,13 +1,19 @@
 #ifndef CHATTINGHISTORYWIDGET_H
 #define CHATTINGHISTORYWIDGET_H
 
+#include "ChattingHistory.hpp"
 #include "listitemwidgetbase.h"
-#include <UserNameCard.h>
-#include <memory>
 
 namespace Ui {
 class ChattingHistoryWidget;
 }
+
+enum class MsgType;
+struct FriendChattingHistory;
+
+struct ChattingTextMsg;
+struct ChattingVoice;
+struct ChattingVideo;
 
 class ChattingHistoryWidget : public ListItemWidgetBase {
   Q_OBJECT
@@ -16,16 +22,15 @@ public:
   ChattingHistoryWidget(QWidget *parent = nullptr);
   virtual ~ChattingHistoryWidget();
 
-  void setUserInfo(std::shared_ptr<UserNameCard> info);
+  void setUserInfo(std::shared_ptr<FriendChattingHistory> info);
   void setLastMsg(const QString &lastmsg);
   void setItemDisplay();
 
-  std::shared_ptr<UserNameCard> getUserInfo();
+  std::shared_ptr<FriendChattingHistory> getChattingContext();
 
 private:
   Ui::ChattingHistoryWidget *ui;
-  std::shared_ptr<UserNameCard> m_userinfo;
-  QString m_lastmsg;
+  std::shared_ptr<FriendChattingHistory> m_userinfo;
 };
 
 #endif // CHATTINGHISTORYWIDGET_H
